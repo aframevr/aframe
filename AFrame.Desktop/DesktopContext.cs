fb071cf771c559dd2cfe586fef175ecc0da2b172
Corey Warner <corey@coreywarner.com>
2015-03-09 07:53:22 +1000

    Added more Launch methods and a FromProcess method to match the ways to set an ApplicationUnderTest

--- AFrame.Desktop/DesktopContext.cs
+++ AFrame.Desktop/DesktopContext.cs
@@ -19,17 +19,17 @@ namespace AFrame.Desktop
             : this(null, null)
         { }
 
-        internal DesktopContext(IContext parentContext, SearchPropertyStack searchParamters)
-            : base(parentContext, searchParamters)
-        {}
+        internal DesktopContext(IContext parentContext, SearchPropertyStack searchParameters)
+            : base(parentContext, searchParameters)
+        { }
 
         public override void Dispose()
         {
-            if(this.ApplicationUnderTest != null)
+            if (this.ApplicationUnderTest != null)
                 this.ApplicationUnderTest.Dispose();
         }
 
-        public T Launch<T>(string appPath) where T : DesktopControl
+        public T Launch<T>(ProcessStartInfo startInfo) where T : DesktopControl
         {
             AFrame.Core.Playback.HighlightOnFind = false;
 
@@ -42,12 +42,12 @@ namespace AFrame.Desktop
             Mouse.MouseDragSpeed = 0;
             Mouse.MouseMoveSpeed = 0;
 
-            this.ApplicationUnderTest = ApplicationUnderTest.Launch(appPath);
+            this.ApplicationUnderTest = ApplicationUnderTest.Launch(startInfo);
 
             return this.As<T>();
         }
 
-        public T Launch<T>(ProcessStartInfo appPath) where T : DesktopControl
+        public T Launch<T>(string fileName) where T : DesktopControl
         {
             AFrame.Core.Playback.HighlightOnFind = false;
 
@@ -60,9 +60,82 @@ namespace AFrame.Desktop
             Mouse.MouseDragSpeed = 0;
             Mouse.MouseMoveSpeed = 0;
 
-            this.ApplicationUnderTest = ApplicationUnderTest.Launch(appPath);
+            this.ApplicationUnderTest = ApplicationUnderTest.Launch(fileName);
 
             return this.As<T>();
         }
+
+        public T Launch<T>(string fileName, string alternativeFileName) where T : DesktopControl
+        {
+            AFrame.Core.Playback.HighlightOnFind = false;
+
+            Microsoft.VisualStudio.TestTools.UITesting.Playback.PlaybackSettings.SmartMatchOptions = Microsoft.VisualStudio.TestTools.UITest.Extension.SmartMatchOptions.None;
+            Microsoft.VisualStudio.TestTools.UITesting.Playback.PlaybackSettings.ShouldSearchFailFast = true;
+            Microsoft.VisualStudio.TestTools.UITesting.Playback.PlaybackSettings.MatchExactHierarchy = true;
+            Microsoft.VisualStudio.TestTools.UITesting.Playback.PlaybackSettings.DelayBetweenActions = 0;
+            Microsoft.VisualStudio.TestTools.UITesting.Playback.PlaybackSettings.SearchTimeout = AFrame.Core.Playback.SearchTimeout;
+
+            Mouse.MouseDragSpeed = 0;
+            Mouse.MouseMoveSpeed = 0;
+
+            this.ApplicationUnderTest = ApplicationUnderTest.Launch(fileName, alternativeFileName);
+
+            return this.As<T>();
+        }
+
+        public T Launch<T>(string fileName, string alternativeFileName, string arguments) where T : DesktopControl
+        {
+            AFrame.Core.Playback.HighlightOnFind = false;
+
+            Microsoft.VisualStudio.TestTools.UITesting.Playback.PlaybackSettings.SmartMatchOptions = Microsoft.VisualStudio.TestTools.UITest.Extension.SmartMatchOptions.None;
+            Microsoft.VisualStudio.TestTools.UITesting.Playback.PlaybackSettings.ShouldSearchFailFast = true;
+            Microsoft.VisualStudio.TestTools.UITesting.Playback.PlaybackSettings.MatchExactHierarchy = true;
+            Microsoft.VisualStudio.TestTools.UITesting.Playback.PlaybackSettings.DelayBetweenActions = 0;
+            Microsoft.VisualStudio.TestTools.UITesting.Playback.PlaybackSettings.SearchTimeout = AFrame.Core.Playback.SearchTimeout;
+
+            Mouse.MouseDragSpeed = 0;
+            Mouse.MouseMoveSpeed = 0;
+
+            this.ApplicationUnderTest = ApplicationUnderTest.Launch(fileName, alternativeFileName, arguments);
+
+            return this.As<T>();
+        }
+
+        public T Launch<T>(string fileName, string alternativeFileName, string arguments, string userName, System.Security.SecureString password, string domain) where T : DesktopControl
+        {
+            AFrame.Core.Playback.HighlightOnFind = false;
+
+            Microsoft.VisualStudio.TestTools.UITesting.Playback.PlaybackSettings.SmartMatchOptions = Microsoft.VisualStudio.TestTools.UITest.Extension.SmartMatchOptions.None;
+            Microsoft.VisualStudio.TestTools.UITesting.Playback.PlaybackSettings.ShouldSearchFailFast = true;
+            Microsoft.VisualStudio.TestTools.UITesting.Playback.PlaybackSettings.MatchExactHierarchy = true;
+            Microsoft.VisualStudio.TestTools.UITesting.Playback.PlaybackSettings.DelayBetweenActions = 0;
+            Microsoft.VisualStudio.TestTools.UITesting.Playback.PlaybackSettings.SearchTimeout = AFrame.Core.Playback.SearchTimeout;
+
+            Mouse.MouseDragSpeed = 0;
+            Mouse.MouseMoveSpeed = 0;
+
+            this.ApplicationUnderTest = ApplicationUnderTest.Launch(fileName, alternativeFileName, arguments, userName, password, domain);
+
+            return this.As<T>();
+        }
+
+        public T FromProcess<T>(Process processToWrap) where T : DesktopControl
+        {
+            AFrame.Core.Playback.HighlightOnFind = false;
+
+            Microsoft.VisualStudio.TestTools.UITesting.Playback.PlaybackSettings.SmartMatchOptions = Microsoft.VisualStudio.TestTools.UITest.Extension.SmartMatchOptions.None;
+            Microsoft.VisualStudio.TestTools.UITesting.Playback.PlaybackSettings.ShouldSearchFailFast = true;
+            Microsoft.VisualStudio.TestTools.UITesting.Playback.PlaybackSettings.MatchExactHierarchy = true;
+            Microsoft.VisualStudio.TestTools.UITesting.Playback.PlaybackSettings.DelayBetweenActions = 0;
+            Microsoft.VisualStudio.TestTools.UITesting.Playback.PlaybackSettings.SearchTimeout = AFrame.Core.Playback.SearchTimeout;
+
+            Mouse.MouseDragSpeed = 0;
+            Mouse.MouseMoveSpeed = 0;
+
+            this.ApplicationUnderTest = ApplicationUnderTest.FromProcess(processToWrap);
+
+            return this.As<T>();
+        }
+
     }
 }
