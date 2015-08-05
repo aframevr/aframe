3171dc75568ca698cc40fc4c48a153e821ec1522
rrukavina <rrukavina@gmail.com>
2015-08-04 18:02:06 -0700

    Fixed nested iframe web element find
    Fixed web element find for the case where the element is within a nested
    iframe.  Also added unit tests.

--- Tests/AFrame.Web.Tests/Features/iFrames/ExtractFrameDataTests.cs
+++ Tests/AFrame.Web.Tests/Features/iFrames/ExtractFrameDataTests.cs
@@ -22,6 +22,20 @@ namespace AFrame.Web.Tests.Features.iFrames
         }
 
         [TestMethod]
+        public void CanExtractNameWithMultipleAttributeSelectorFromStringWithPreAndPost()
+        {
+            var jquery = ".blah [iframe=iframe[name1='.loginFrame #id'][name2='value2']] #testing";
+
+            var extracts = Helpers.ExtractiFrameData(jquery).ToList();
+
+            Assert.AreEqual(2, extracts.Count());
+            Assert.AreEqual(".blah ", extracts[0].jQuery);
+            Assert.AreEqual("iframe[name1='.loginFrame #id'][name2='value2']", extracts[0].jQuerySelector);
+            Assert.AreEqual(" #testing", extracts[1].jQuery);
+            Assert.AreEqual("", extracts[1].jQuerySelector);
+        }
+
+        [TestMethod]
         public void CanExtractNameFromStringWithPre()
         {
             var jquery = ".blah [iFrame='.loginframe']";
@@ -73,6 +87,38 @@ namespace AFrame.Web.Tests.Features.iFrames
             Assert.AreEqual("#loginFrame #blah", extracts[0].jQuerySelector);
             Assert.AreEqual(" #testing", extracts[1].jQuery);
             Assert.AreEqual("", extracts[1].jQuerySelector);
+        }
+
+        [TestMethod]
+        public void CanExtractMultipleIframes()
+        {
+            var jquery = "#firstid [iframe='aFrame'] #secondId [iframe='loginFrame'] .inner-contents-of-frame";
+
+            var extracts = Helpers.ExtractiFrameData(jquery).ToList();
+
+            Assert.AreEqual(3, extracts.Count());
+            Assert.AreEqual("#firstid ", extracts[0].jQuery);
+            Assert.AreEqual("aFrame", extracts[0].jQuerySelector);
+            Assert.AreEqual(" #secondId ", extracts[1].jQuery);
+            Assert.AreEqual("loginFrame", extracts[1].jQuerySelector);
+            Assert.AreEqual(" .inner-contents-of-frame", extracts[2].jQuery);
+            Assert.AreEqual("", extracts[2].jQuerySelector);
+        }
+
+        [TestMethod]
+        public void CanExtractMultipleIframesWithOnlyNestedIframeInnerContentsJquery()
+        {
+            var jquery = "[iframe='aFrame'] [iframe='loginFrame'] .inner-contents-of-frame";
+
+            var extracts = Helpers.ExtractiFrameData(jquery).ToList();
+
+            Assert.AreEqual(3, extracts.Count());
+            Assert.AreEqual("", extracts[0].jQuery);
+            Assert.AreEqual("aFrame", extracts[0].jQuerySelector);
+            Assert.AreEqual(" ", extracts[1].jQuery);
+            Assert.AreEqual("loginFrame", extracts[1].jQuerySelector);
+            Assert.AreEqual(" .inner-contents-of-frame", extracts[2].jQuery);
+            Assert.AreEqual("", extracts[2].jQuerySelector);
         }
     }
 }
