985b5306e7d184147126edee10384bf50d8428fa
Josh Carpenter <jcarpenter@mozilla.com>
2015-08-27 15:17:33 -0700

    Added skysphere

--- components/vr-skysphere.js
+++ components/vr-skysphere.js
@@ -2,7 +2,7 @@
 (function(define){'use strict';define(function(require,exports,module){
 
   document.registerElement(
-    'vr-image',
+    'vr-skysphere',
     {
       prototype: Object.create(
         VRObject.prototype, {
@@ -20,22 +20,21 @@
               var material = this.getMaterial();
               var geometry = this.getGeometry();
               this.object3D.geometry = geometry;
-              this.object3D.material = material;            
-            }
+              this.object3D.material = material;
+            }            
           },
 
           getGeometry: {
             value: function() {
-              var width = parseFloat(this.getAttribute('width')) || 10;
-              var height = parseFloat(this.getAttribute('height')) || 10;
-              return new THREE.PlaneGeometry( width, height, 1, 1 );
+              var radius = parseFloat(this.getAttribute('radius')) || 5000;
+              return new THREE.SphereGeometry( radius, 64, 40 );
             }
           },
 
           getMaterial: {
             value: function() {
               var imgSrc = this.getAttribute('src');
-              return new THREE.MeshBasicMaterial({ map: THREE.ImageUtils.loadTexture(imgSrc), side: THREE.DoubleSide });
+              return new THREE.MeshBasicMaterial({ map: THREE.ImageUtils.loadTexture(imgSrc), side: THREE.BackSide });
             }
           }
         })
@@ -43,9 +42,9 @@
   );
 
   var VRTags = window.VRTags = window.VRTags || {};
-  VRTags["VR-IMAGE"] = true;
+  VRTags["VR-SKYSPHERE"] = true;
 
 });})(typeof define=='function'&&define.amd?define
 :(function(n,w){'use strict';return typeof module=='object'?function(c){
 c(require,exports,module);}:function(c){var m={exports:{}};c(function(n){
-return w[n];},m.exports,m);w[n]=m.exports;};})('VRImage',this));
+return w[n];},m.exports,m);w[n]=m.exports;};})('VRSkysphere',this));
