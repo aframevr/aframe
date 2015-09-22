0d4c2ed330b189dcd60b09ff059b29eaea63b6f1
Diego Marcos <diego.marcos@gmail.com>
2015-09-22 10:19:43 -0700

    Renaming files

---
+++ src/components/components.js
@@ -0,0 +1,3 @@
+module.exports.camera = require('./camera');
+module.exports.geometry = require('./geometry');
+module.exports.material = require('./material');
--- src/components/vr-components.js
+++
@@ -1,3 +0,0 @@
-module.exports.camera = require('./vr-camera');
-module.exports.geometry = require('./vr-geometry');
-module.exports.material = require('./vr-material');
