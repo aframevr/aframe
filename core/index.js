7d1acac84e99612ee4169f1ccc4da75a28cb432e
Christopher Van <cvan@mozilla.com>
2015-10-19 15:43:36 -0700

    introduce template bundler (fixes #67) and cube template (fixes #49)

--- core/index.js
+++ core/index.js
@@ -1,3 +1,16 @@
-module.exports = {
-  'vr-template': require('./vr-template')
+var modules = {
+  'vr-template': require('./vr-template'),
+  'vr-cube': require('./templates/vr-cube.html')
 };
+
+// Inline all HTML templates so we don't have to slowly import them all.
+document.addEventListener('vr-markup-ready', function () {
+  var vrSceneEl = document.querySelector('vr-scene');
+  Object.keys(modules).forEach(function (key) {
+    if (typeof modules[key] === 'string') {
+      vrSceneEl.insertAdjacentHTML('afterend', modules[key]);
+    }
+  });
+});
+
+module.exports = modules;
