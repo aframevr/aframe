eef5b16c991fe2fff9b13358f8d3d3e934e48be4
Christopher Van <cvan@mozilla.com>
2015-10-28 14:28:52 -0700

    create `vr-clicked` and `vr-hovered`

---
+++ core/vr-event/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  'vr-click': require('./vr-click'),
+  'vr-hover': require('./vr-hover'),
+  'vr-mousedown': require('./vr-mousedown'),
+  'vr-mouseenter': require('./vr-mouseenter'),
+  'vr-mouseleave': require('./vr-mouseleave'),
+  'vr-mouseup': require('./vr-mouseup')
+};
