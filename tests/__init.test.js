aaf60fd5649e54f7f825c8ace5b9c6eac788cbb4
Kevin Ngo <me@ngokevin.com>
2015-11-16 20:37:14 -0800

    organize/jsdoc scene file

--- tests/__init.test.js
+++ tests/__init.test.js
@@ -6,13 +6,10 @@
 window.debug = true;
 
 var VRScene = require('core/vr-scene');
-var THREE = require('vr-markup').THREE;
 
 setup(function () {
   this.sinon = sinon.sandbox.create();
-  this.sinon.stub(VRScene.prototype, 'attachedCallback', function () {
-    this.object3D = new THREE.Scene();
-  });
+  this.sinon.stub(VRScene.prototype, 'attachedCallback');
 });
 
 teardown(function () {
