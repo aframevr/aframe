2cffca8181dadfb3c21211dfb56680bb60b2b255
Don McCurdy <don.r.mccurdy@gmail.com>
2015-12-19 11:26:31 -0500

    Add MTLLoader, with support for .obj+.mtl pairs.

--- src/components/loader.js
+++ src/components/loader.js
@@ -10,6 +10,7 @@ module.exports.Component = registerComponent('loader', {
 
   schema: {
     src: { default: '' },
+    mtl: { default: '' },
     format: {
       default: 'obj',
       oneOf: ['obj', 'collada']
@@ -21,6 +22,7 @@ module.exports.Component = registerComponent('loader', {
     var data = this.data;
     var model = this.model;
     var url = parseUrl(data.src);
+    var mtlUrl = parseUrl(data.mtl);
     var format = data.format;
     if (model) { el.object3D.remove(model); }
     if (!url) {
@@ -29,7 +31,7 @@ module.exports.Component = registerComponent('loader', {
     }
     switch (format) {
       case 'obj':
-        this.loadObj(url);
+        this.loadObj(url, mtlUrl);
         break;
       case 'collada':
         this.loadCollada(url);
@@ -39,15 +41,33 @@ module.exports.Component = registerComponent('loader', {
     }
   },
 
-  loadObj: function (url) {
+  loadObj: function (objUrl, mtlUrl) {
     var self = this;
     var el = this.el;
-    var loader = new THREE.OBJLoader();
-    loader.load(url, function (object) {
-      self.model = object;
-      self.applyMaterial();
-      el.object3D.add(object);
-    });
+    var objLoader = new THREE.OBJLoader();
+    if (mtlUrl) {
+      // .OBJ + .MTL assets.
+      if (el.components.material) {
+        warn('Material component is ignored when a .MTL is provided');
+      }
+      var mtlLoader = new THREE.MTLLoader(objLoader.manager);
+      mtlLoader.setBaseUrl(mtlUrl.substr(0, mtlUrl.lastIndexOf('/') + 1));
+      mtlLoader.load(mtlUrl, function (materials) {
+        materials.preload();
+        objLoader.setMaterials(materials);
+        objLoader.load(objUrl, function (object) {
+          self.model = object;
+          el.object3D.add(object);
+        });
+      });
+    } else {
+      // .OBJ asset only.
+      objLoader.load(objUrl, function (object) {
+        self.model = object;
+        self.applyMaterial();
+        el.object3D.add(object);
+      });
+    }
   },
 
   applyMaterial: function () {
