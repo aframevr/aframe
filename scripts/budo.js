8f2c018454b9d382b21d509288fdf5145a8a91c0
Kevin Ngo <me@ngokevin.com>
2016-01-13 14:21:10 -0800

    restore budo upgrade

--- scripts/budo.js
+++ scripts/budo.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+
 var exec = require('child_process').exec;
 var urlParse = require('url').parse;
 
@@ -14,24 +15,24 @@ function execCmd (cmd) {
 var consts = {
   NAME: 'AFRAME',
   ENTRY: './src/index.js',
-  DIST: 'dist/aframe-core.js',
-  BUILD: 'build/aframe-core.js',
-  WATCH: '{examples,lib,src,style}/**/*',
-  PORT: 9001
+  DIST: 'dist/aframe.js',
+  BUILD: 'build/aframe.js',
+  WATCH: 'examples/**/*',  // Additional files to watch for LiveReload
+  PORT: 9000
 };
 
 var opts = {
   debug: process.env.NODE_ENVIRONMENT !== 'production',
   verbose: true,
-  open: process.env.OPEN,  // To enable: `OPEN=1 npm start`
-  live: process.env.LIVE,  // To enable: `LIVE=1 npm start`
+  live: true,
   stream: process.stdout,
   host: process.env.HOST,
   port: process.env.PORT || consts.PORT,
+  watchGlob: consts.WATCH,
   browserifyArgs: ['-s', consts.NAME],
   middleware: function (req, res, next) {
-    // Route `dist/aframe-core.js` to `build/aframe-core.js`
-    // so we can dev against the examples :)
+    // Route `dist/aframe.js` to `build/aframe.js` so we can
+    // dev against the examples :)
     var path = urlParse(req.url).pathname;
     if (path.indexOf('/' + consts.DIST) !== -1) {
       req.url = req.url.replace('/dist/', '/build/');
@@ -43,19 +44,6 @@ var opts = {
 };
 
 var app = budo(consts.ENTRY + ':' + consts.BUILD, opts);
-
-app.watch(consts.WATCH)
-.on('update', function () {
-  execCmd('semistandard -v');
+app.on('update', function () {
+  execCmd('semistandard -v $(git ls-files "*.js") | snazzy');
 });
-
-if (opts.live) {
-  app.live()
-  .on('watch', function (eventType, fn) {
-    if (eventType !== 'change' && eventType !== 'add') { return; }
-    app.reload(fn);
-  })
-  .on('pending', function () {
-    app.reload(opts.output);
-  });
-}
