7de32257ff92719adc2a12332a1fb1ae86194bd6
Christopher Van <cvan@mozilla.com>
2016-01-20 20:16:04 -0800

    allow cross-origin images to be loaded from model loaders (fixes #870)

--- src/lib/three.js
+++ src/lib/three.js
@@ -1,8 +1,16 @@
 var THREE = global.THREE = require('three-dev');
 
 // Allow cross-origin images to be loaded.
+
+// This should not be on `THREE.Loader` nor `THREE.ImageUtils`.
+// Must be on `THREE.TextureLoader`.
 if (THREE.TextureLoader) {
   THREE.TextureLoader.prototype.crossOrigin = '';
+}
+
+// This is for images loaded from the model loaders.
+if (THREE.ImageLoader) {
+  THREE.ImageLoader.prototype.crossOrigin = '';
 }
 
 // TODO: Eventually include these only if they are needed by a component.
