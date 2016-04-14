755686c5e6e87ddc6d64adfd417a1e98f563d9c3
Don McCurdy <don.r.mccurdy@gmail.com>
2016-04-13 23:04:03 -0400

    Add tests on "hmd-controls" component.

---
+++ tests/components/controls/hmd-controls.test.js
@@ -0,0 +1,54 @@
+/* global assert, process, setup, suite, test */
+'use strict';
+var entityFactory = require('../../helpers').entityFactory;
+var THREE = require('index').THREE;
+var EPS = 10e-6;
+
+suite('hmd-controls', function () {
+  var hmd, hmdControls;
+
+  setup(function (done) {
+    var el = this.el = entityFactory();
+    hmd = {
+      dolly: null,
+      update: this.sinon.spy(),
+      dispose: this.sinon.spy()
+    };
+    this.sinon.stub(THREE, 'VRControls', function (dolly) {
+      hmd.dolly = hmd.dolly || dolly;
+      return hmd;
+    });
+    el.setAttribute('hmd-controls', '');
+    el.addEventListener('loaded', function () {
+      hmdControls = el.components['hmd-controls'];
+      done();
+    });
+  });
+
+  suite('isRotationActive', function () {
+    test('not active by default', function () {
+      assert.isFalse(hmdControls.isRotationActive());
+    });
+
+    test('active when HMD has moved', function () {
+      hmd.dolly.quaternion.set(Math.PI / 4, 0, 0, 1);
+      assert.isTrue(hmdControls.isRotationActive());
+    });
+
+    test('inactive when disabled', function () {
+      hmd.dolly.quaternion.set(Math.PI / 4, 0, 0, 1);
+      this.el.setAttribute('hmd-controls', {enabled: false});
+      assert.isFalse(hmdControls.isRotationActive());
+    });
+  });
+
+  suite('getRotation', function () {
+    test('returns HMD rotation', function () {
+      hmd.dolly.quaternion.set(0.5, 0, 0, 0.8660254);
+      var rotation = hmdControls.getRotation();
+      assert.approximately(rotation.x, 60, EPS);
+      assert.equal(rotation.y, 0);
+      assert.equal(rotation.z, 0);
+    });
+  });
+});
