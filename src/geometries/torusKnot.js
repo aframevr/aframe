5728bf51b40b3a2eb3c86aa61e245bfc72dfae87
Fernando Serrano <fernandojsg@gmail.com>
2016-05-27 19:14:15 +0200

    Fixed typo in 'default' torusknot' schema

--- src/geometries/torusKnot.js
+++ src/geometries/torusKnot.js
@@ -7,7 +7,7 @@ registerGeometry('torusKnot', {
     q: {default: 3, min: 1, type: 'int'},
     radius: {default: 1, min: 0},
     radiusTubular: {default: 0.2, min: 0},
-    segmentsRadial: {efault: 36, min: 3, type: 'int'},
+    segmentsRadial: {default: 36, min: 3, type: 'int'},
     segmentsTubular: {default: 100, min: 3, type: 'int'}
   },
 
