b3243017a133d200ce6e65c13f3ff41f69f4ba60
Fernando Serrano <fernandojsg@gmail.com>
2016-05-27 11:13:59 +0200

    Correct geometry schemas and add segments parameters for box and plane

--- src/geometries/box.js
+++ src/geometries/box.js
@@ -5,7 +5,10 @@ registerGeometry('box', {
   schema: {
     depth: {default: 1, min: 0},
     height: {default: 1, min: 0},
-    width: {default: 1, min: 0}
+    width: {default: 1, min: 0},
+    segmentsHeight: {default: 1, min: 1, max: 20, type: 'int'},
+    segmentsWidth: {default: 1, min: 1, max: 20, type: 'int'},
+    segmentsDepth: {default: 1, min: 1, max: 20, type: 'int'}
   },
 
   init: function (data) {
--- src/geometries/octahedron.js
+++ src/geometries/octahedron.js
@@ -3,7 +3,7 @@ var THREE = require('../lib/three');
 
 registerGeometry('octahedron', {
   schema: {
-    detail: {default: 0, min: 0, max: 1},
+    detail: {default: 0, min: 0, max: 5, type: 'int'},
     radius: {default: 1, min: 0}
   },
 
