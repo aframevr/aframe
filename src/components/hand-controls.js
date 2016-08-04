a2a41e5c014734f40a2ab3fd4dd858880eecea5b
bryik <ben@wsundine.com>
2016-08-03 22:18:07 -0400

    Fix hand-controls events being attached twice
    Also renamed "stop()" to "pause()" and corrected a typo in the component docstring.

--- src/components/hand-controls.js
+++ src/components/hand-controls.js
@@ -5,7 +5,7 @@ var RIGHT_HAND_MODEL_URL = 'https://media.aframe.io/controllers/hands/rightHand.
 /**
 *
 * Hand Controls component
-* HAndle events coming from the vive-controls
+* Handle events coming from the vive-controls
 * Translate button events to hand related actions:
 * gripclose, gripopen, thumbup, thumbdown, pointup, pointdown
 * Load a hand model with gestures that are applied based
@@ -24,14 +24,13 @@ module.exports.Component = registerComponent('hand-controls', {
     this.onTrackpadUp = function () { self.handleButton('trackpad', 'up'); };
     this.onTriggerDown = function () { self.handleButton('trigger', 'down'); };
     this.onTriggerUp = function () { self.handleButton('trigger', 'up'); };
-    this.addEventListeners();
   },
 
   play: function () {
     this.addEventListeners();
   },
 
-  stop: function () {
+  pause: function () {
     this.removeEventListeners();
   },
 
