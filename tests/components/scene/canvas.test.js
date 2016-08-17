fff5d476e66109afc3b2ff28207d6701ae406a13
Kevin Ngo <me@ngokevin.com>
2016-08-16 18:17:45 -0700

    add unit tests for scene enter/exit VR

--- tests/components/scene/canvas.test.js
+++ tests/components/scene/canvas.test.js
@@ -1,7 +1,5 @@
  /* global assert, process, suite, test */
 
-var FULLSCREEN_CLASS = 'fullscreen';
-
 suite('canvas', function () {
   test('adds canvas to a-scene element', function (done) {
     var el = this.sceneEl = document.createElement('a-scene');
@@ -10,21 +8,6 @@ suite('canvas', function () {
       el.setAttribute('canvas', '');
       assert.ok(el.querySelector('canvas'));
       done();
-    });
-  });
-
-  suite('fullscreen', function () {
-    test('adds fullscreen class on enter VR', function () {
-      var el = this.sceneEl;
-      el.emit('enter-vr');
-      assert.ok(el.canvas.classList.contains(FULLSCREEN_CLASS));
-    });
-
-    test('removes fullscreen class on exit VR', function () {
-      var el = this.sceneEl;
-      el.canvas.classList.add(FULLSCREEN_CLASS);
-      el.emit('exit-vr');
-      assert.notOk(el.canvas.classList.contains(FULLSCREEN_CLASS));
     });
   });
 });
