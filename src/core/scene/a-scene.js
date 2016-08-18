54589198f436576160b84522f2ca4abeef8d39da
Diego Marcos <diego.marcos@gmail.com>
2016-08-17 20:22:53 -0700

    Shortcut to inject the editor (#1599)
    * Shortcut to inject the editor
    
    * Bump build

--- src/core/scene/a-scene.js
+++ src/core/scene/a-scene.js
@@ -38,6 +38,7 @@ module.exports = registerElement('a-scene', {
     defaultComponents: {
       value: {
         'canvas': '',
+        'inspector': '',
         'keyboard-shortcuts': '',
         'vr-mode-ui': ''
       }
