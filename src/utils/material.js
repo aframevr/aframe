2ef41755afb43dfa8de3ad938a925c58101386df
Ada Rose Edwards <ada@ada.is>
2016-08-19 13:14:06 +0100

    Add more physical maps to stadard material
    fix docstrings

--- src/utils/material.js
+++ src/utils/material.js
@@ -1,3 +1,5 @@
+var THREE = require('../lib/three');
+
 /**
  * Update `material.map` given `data.src`. For standard and flat shaders.
  *
@@ -23,6 +25,55 @@ module.exports.updateMap = function (shader, data) {
 
   function setMap (texture) {
     material.map = texture;
+    material.needsUpdate = true;
+    handleTextureEvents(el, texture);
+  }
+};
+
+/**
+ * Update's the material's maps which give the illusion of extra geometry.
+ *
+ * @param {string} longType - The friendly name of the map from the component e.g. ambientOcclusionMap becomes aoMap in THREE.js
+ * @param {object} shader - A-Frame shader instance
+ * @param {object} data
+ */
+module.exports.updateDistortionMap = function (longType, shader, data) {
+  var shortType = longType;
+  if (longType === 'ambientOcclusion') { shortType = 'ao'; }
+  var el = shader.el;
+  var material = shader.material;
+  var src = data[longType + 'Map'];
+  var info = {};
+  info[longType + 'Map'] = src;
+
+  if (src) {
+    if (src === shader[longType + 'TextureSrc']) { return; }
+
+    // Texture added or changed.
+    shader[longType + 'TextureSrc'] = src;
+    el.sceneEl.systems.material.loadTexture(src, info, setMap);
+    return;
+  }
+
+  // Texture removed.
+  if (!material.map) { return; }
+  setMap(null);
+
+  function setMap (texture) {
+    if (data[longType + 'TextureWrap'] === 'repeat') {
+      texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+    } else if (data[longType + 'TextureWrap'] === 'clamp') {
+      texture.wrapS = texture.wrapT = THREE.ClampToEdgeWrapping;
+    } else if (data[longType + 'TextureWrap'] === 'mirrored') {
+      texture.wrapS = texture.wrapT = THREE.MirroredRepeatWrapping;
+    } else {
+      texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
+    }
+
+    texture.offset = data[longType + 'TextureOffset'];
+    texture.repeat = (new THREE.Vector2()).copy(data[longType + 'TextureRepeat']);
+
+    material[shortType + 'Map'] = texture;
     material.needsUpdate = true;
     handleTextureEvents(el, texture);
   }
