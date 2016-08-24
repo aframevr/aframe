a2d4d106e45f7fc59fc318cf08449fc7a26e995b
David Walsh <davidwalsh83@gmail.com>
2016-08-23 20:45:41 -0500

    Fix #1817 - Add basic test for inspector

--- src/components/scene/inspector.js
+++ src/components/scene/inspector.js
@@ -1,6 +1,7 @@
 /* global AFRAME */
-var registerComponent = require('../../core/component').registerComponent;
+var AFRAME_INJECTED = require('../../constants').AFRAME_INJECTED;
 var pkg = require('../../../package');
+var registerComponent = require('../../core/component').registerComponent;
 
 var INSPECTOR_URL = pkg.homepage + 'releases/' + pkg.version + '/aframe-inspector.min.js';
 
@@ -30,6 +31,8 @@ module.exports.Component = registerComponent('inspector', {
     // Inject.
     script = document.createElement('script');
     script.src = this.data.url;
+    script.setAttribute('data-name', 'aframe-inspector');
+    script.setAttribute(AFRAME_INJECTED, '');
     script.onload = function () {
       AFRAME.INSPECTOR.open();
       window.removeEventListener('keydown', self.onKeydown);
