ffdabd9673939285323290f76601734c120dcde8
Kevin Ngo <me@ngokevin.com>
2016-10-23 17:14:30 -0700

    allow opening inspector through postMessage

--- src/components/scene/inspector.js
+++ src/components/scene/inspector.js
@@ -13,14 +13,29 @@ module.exports.Component = registerComponent('inspector', {
 
   init: function () {
     this.onKeydown = bind(this.onKeydown, this);
+    this.onMessage = bind(this.onMessage, this);
     window.addEventListener('keydown', this.onKeydown);
+    window.addEventListener('message', this.onMessage);
   },
 
+  remove: function () {
+    this.removeEventListeners();
+  },
+
+  /**
+   * <ctrl> + <alt> + i keyboard shortcut.
+   */
   onKeydown: function (evt) {
-    // Alt + Ctrl + i
     var shortcutPressed = evt.keyCode === 73 && evt.ctrlKey && evt.altKey;
     if (!this.data || !shortcutPressed) { return; }
     this.injectInspector();
+  },
+
+  /**
+   * postMessage. aframe.io uses this to create a button on examples to open Inspector.
+   */
+  onMessage: function (evt) {
+    if (evt.data === 'INJECT_AFRAME_INSPECTOR') { this.injectInspector(); }
   },
 
   injectInspector: function () {
@@ -36,13 +51,14 @@ module.exports.Component = registerComponent('inspector', {
     script.setAttribute(AFRAME_INJECTED, '');
     script.onload = function () {
       AFRAME.INSPECTOR.open();
-      window.removeEventListener('keydown', self.onKeydown);
+      self.removeEventListeners();
     };
     document.head.appendChild(script);
     AFRAME.inspectorInjected = true;
   },
 
-  remove: function () {
+  removeEventListeners: function () {
     window.removeEventListener('keydown', this.onKeydown);
+    window.removeEventListener('message', this.onMessage);
   }
 });
