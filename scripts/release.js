43ee235cebcdc542d5f4c42211d351002c182fc8
Kevin Ngo <me@ngokevin.com>
2016-11-11 15:23:24 -0800

    release helper script

--- scripts/release.js
+++ scripts/release.js
@@ -4,13 +4,19 @@ var glob = require('glob');
 var prevVersion = process.argv[2];
 var nextVersion = process.argv[3];
 
+if (!prevVersion || !nextVersion) {
+  console.error('Error: you must pass in the old version and new version: ' +
+                '(e.g., `node scripts/release.js 0.3.0 0.4.0`)');
+  process.exit(1);
+}
+
 // Copy `aframe.js` to `aframe-v{newVersion}js`.
-glob.sync('dist/aframe*', {
+glob.sync('dist/aframe-master*', {
   exclude: `dist/aframe*v${prevVersion}*`
 }).forEach(function copyMasterToStable (masterFilename) {
-  var stableFilename = masterFilename.replace('aframe', `aframe-v${nextVersion}`);
+  var stableFilename = masterFilename.replace('master', `v${nextVersion}`);
   fs.createReadStream(masterFilename).pipe(fs.createWriteStream(stableFilename));
 });
 
 // Remove `aframe-v{prevVersion}.js`.
-glob.sync(`dist/aframe*v${nextVersion}`).forEach(fs.unlinkSync);
+glob.sync(`dist/aframe*v${prevVersion}*`).forEach(fs.unlinkSync);
