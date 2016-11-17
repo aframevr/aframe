509513a83df39394cfd33c0a009bf549d896a3ba
Kevin Ngo <me@ngokevin.com>
2016-11-16 18:21:18 -0800

    fix missing primitive mappings for core material props

--- src/extras/primitives/getMeshMixin.js
+++ src/extras/primitives/getMeshMixin.js
@@ -1,15 +1,19 @@
 /**
  * Common mesh defaults, mappings, and transforms.
  */
+var components = require('../../core/component').components;
 var shaders = require('../../core/shader').shaders;
 var utils = require('../../utils/');
 
 var materialMappings = {};
-Object.keys(shaders.standard.schema).forEach(function addMapping (prop) {
+Object.keys(components.material.schema).forEach(addMapping);
+Object.keys(shaders.standard.schema).forEach(addMapping);
+
+function addMapping (prop) {
   // To hyphenated.
   var htmlAttrName = prop.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
   materialMappings[htmlAttrName] = 'material.' + prop;
-});
+}
 
 module.exports = function getMeshMixin () {
   return {
