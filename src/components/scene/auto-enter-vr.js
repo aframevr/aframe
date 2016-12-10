298c3e1b1962c596b8da1bf84ad2d7ff6d3d7a9e
machenmusik <machenmusik@gmail.com>
2016-12-10 17:56:48 -0500

    remove console logs and superfluous code

--- src/components/scene/auto-enter-vr.js
+++ src/components/scene/auto-enter-vr.js
@@ -13,21 +13,11 @@ module.exports.Component = registerComponent('auto-enter-vr', {
     if (utils.getUrlParameter('auto-enter-vr') === 'false') { return; }
 
     window.addEventListener('vrdisplayactivate', function () {
-      console.log('vrdisplayactivate, enterVR()');
       scene.enterVR();
     }, false);
 
-    this.el.addEventListener('enter-vr', function () {
-      console.log('enter-vr');
-    }, false);
-
     window.addEventListener('vrdisplaydeactivate', function () {
-      console.log('vrdisplaydeactivate, exitVR()');
       scene.exitVR();
-    }, false);
-
-    this.el.addEventListener('exit-vr', function () {
-      console.log('exit-vr');
     }, false);
 
     // just try to enter... turns out we need to wait for next tick
