492bacb9dcae839c3f619b3e87bdedd5ff705d57
machenmusik <machenmusik@gmail.com>
2016-12-16 12:43:18 -0500

    added tests

--- tests/components/scene/stats.test.js
+++ tests/components/scene/stats.test.js
@@ -26,4 +26,38 @@ suite('stats', function () {
     assert.equal(this.sceneEl.components.stats.statsEl, undefined);
     assert.equal(this.sceneEl.components.stats.update(), undefined); // Neither show or hide
   });
+
+  suite('stats=true', function () {
+    var originalGetUrlParameter = utils.getUrlParameter;
+
+    setup(function (done) {
+      var el = this.sceneEl = document.createElement('a-scene');
+      el.setAttribute('stats', '');
+      document.body.appendChild(el);
+
+      el.addEventListener('loaded', function () { done(); });
+
+      utils.getUrlParameter = function () { return 'true'; };
+    });
+
+    teardown(function () {
+      var el = this.sceneEl;
+      el.parentNode.removeChild(el);
+
+      utils.getUrlParameter = originalGetUrlParameter;
+    });
+
+    test('Stats are created when query string "stats" is "true"', function () {
+      assert.equal(document.querySelector('.rs-base'), this.sceneEl.components.stats.statsEl);
+      assert.ok(this.sceneEl.components.stats.statsEl);
+    });
+
+    test('Stats DOM element is not visible', function () {
+      assert.equal(this.sceneEl.components.stats.statsEl.style.display, 'none');
+    });
+
+    test('Stats A-Frame element is attached', function () {
+      setTimeout(function () { assert.ok(this.sceneEl.components.stats.statspanel); }, 0);
+    });
+  });
 });
