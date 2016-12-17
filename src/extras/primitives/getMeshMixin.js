4968945151c0316ef68921a7aeb4548e55e571cc
Kevin Ngo <me@ngokevin.com>
2016-12-16 23:09:13 -0800

    get rid of most primitive mapping warnings

--- src/extras/primitives/getMeshMixin.js
+++ src/extras/primitives/getMeshMixin.js
@@ -12,6 +12,8 @@ Object.keys(shaders.standard.schema).forEach(addMapping);
 function addMapping (prop) {
   // To hyphenated.
   var htmlAttrName = prop.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
+  if (prop === 'fog') { htmlAttrName = 'material-fog'; }
+  if (prop === 'visible') { htmlAttrName = 'material-visible'; }
   materialMappings[htmlAttrName] = 'material.' + prop;
 }
 
