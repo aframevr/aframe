24850cc08370f7b7572f3fae73f88ce3161b17e1
dmarcos <diego.marcos@gmail.com>
2017-01-25 15:51:40 -0800

    Attaches event listeners to the controllers controls regardless of the presence of physical tracked controllers

--- tests/components/oculus-touch-controls.test.js
+++ tests/components/oculus-touch-controls.test.js
@@ -17,9 +17,7 @@ suite(controllerComponentName, function () {
     test('first-time, if no controllers, remove event listeners and remember not present', function () {
       var el = this.el;
       var controllerComponent = el.components[controllerComponentName];
-      var addEventListenersSpy = this.sinon.spy(controllerComponent, 'addEventListeners');
       var injectTrackedControlsSpy = this.sinon.spy(controllerComponent, 'injectTrackedControls');
-      var removeEventListenersSpy = this.sinon.spy(controllerComponent, 'removeEventListeners');
       // mock isControllerPresent to return false
       controllerComponent.isControllerPresentMockValue = false;
       // reset so we don't think we've looked before
@@ -28,8 +26,6 @@ suite(controllerComponentName, function () {
       controllerComponent.checkIfControllerPresent();
       // check assertions
       assert.notOk(injectTrackedControlsSpy.called);
-      assert.notOk(addEventListenersSpy.called);
-      assert.ok(removeEventListenersSpy.called);
       assert.ok(controllerComponent.controllerPresent === false); // not undefined
     });
 
@@ -38,7 +34,6 @@ suite(controllerComponentName, function () {
       var controllerComponent = el.components[controllerComponentName];
       var addEventListenersSpy = this.sinon.spy(controllerComponent, 'addEventListeners');
       var injectTrackedControlsSpy = this.sinon.spy(controllerComponent, 'injectTrackedControls');
-      var removeEventListenersSpy = this.sinon.spy(controllerComponent, 'removeEventListeners');
       // mock isControllerPresent to return false
       controllerComponent.isControllerPresentMockValue = false;
       // pretend we've looked before
@@ -48,16 +43,13 @@ suite(controllerComponentName, function () {
       // check assertions
       assert.notOk(injectTrackedControlsSpy.called);
       assert.notOk(addEventListenersSpy.called);
-      assert.notOk(removeEventListenersSpy.called);
       assert.ok(controllerComponent.controllerPresent === false); // not undefined
     });
 
     test('attach events if controller is newly present', function () {
       var el = this.el;
       var controllerComponent = el.components[controllerComponentName];
-      var addEventListenersSpy = this.sinon.spy(controllerComponent, 'addEventListeners');
       var injectTrackedControlsSpy = this.sinon.spy(controllerComponent, 'injectTrackedControls');
-      var removeEventListenersSpy = this.sinon.spy(controllerComponent, 'removeEventListeners');
       // mock isControllerPresent to return true
       controllerComponent.isControllerPresentMockValue = true;
       // reset so we don't think we've looked before
@@ -66,17 +58,13 @@ suite(controllerComponentName, function () {
       controllerComponent.checkIfControllerPresent();
       // check assertions
       assert.ok(injectTrackedControlsSpy.called);
-      assert.ok(addEventListenersSpy.called);
-      assert.notOk(removeEventListenersSpy.called);
       assert.ok(controllerComponent.controllerPresent);
     });
 
     test('do not inject or attach events again if controller is already present', function () {
       var el = this.el;
       var controllerComponent = el.components[controllerComponentName];
-      var addEventListenersSpy = this.sinon.spy(controllerComponent, 'addEventListeners');
       var injectTrackedControlsSpy = this.sinon.spy(controllerComponent, 'injectTrackedControls');
-      var removeEventListenersSpy = this.sinon.spy(controllerComponent, 'removeEventListeners');
       // mock isControllerPresent to return true
       controllerComponent.isControllerPresentMockValue = true;
       // pretend we've looked before
@@ -85,17 +73,13 @@ suite(controllerComponentName, function () {
       controllerComponent.checkIfControllerPresent();
       // check assertions
       assert.notOk(injectTrackedControlsSpy.called);
-      assert.notOk(addEventListenersSpy.called);
-      assert.notOk(removeEventListenersSpy.called);
       assert.ok(controllerComponent.controllerPresent);
     });
 
     test('if controller disappears, remove event listeners', function () {
       var el = this.el;
       var controllerComponent = el.components[controllerComponentName];
-      var addEventListenersSpy = this.sinon.spy(controllerComponent, 'addEventListeners');
       var injectTrackedControlsSpy = this.sinon.spy(controllerComponent, 'injectTrackedControls');
-      var removeEventListenersSpy = this.sinon.spy(controllerComponent, 'removeEventListeners');
       // mock isControllerPresent to return true
       controllerComponent.isControllerPresentMockValue = false;
       // pretend we've looked before
@@ -104,8 +88,6 @@ suite(controllerComponentName, function () {
       controllerComponent.checkIfControllerPresent();
       // check assertions
       assert.notOk(injectTrackedControlsSpy.called);
-      assert.notOk(addEventListenersSpy.called);
-      assert.ok(removeEventListenersSpy.called);
       assert.notOk(controllerComponent.controllerPresent);
     });
   });
--- tests/components/vive-controls.test.js
+++ tests/components/vive-controls.test.js
@@ -17,9 +17,7 @@ suite(controllerComponentName, function () {
     test('first-time, if no controllers, remove event listeners and remember not present', function () {
       var el = this.el;
       var controllerComponent = el.components[controllerComponentName];
-      var addEventListenersSpy = this.sinon.spy(controllerComponent, 'addEventListeners');
       var injectTrackedControlsSpy = this.sinon.spy(controllerComponent, 'injectTrackedControls');
-      var removeEventListenersSpy = this.sinon.spy(controllerComponent, 'removeEventListeners');
       // mock isControllerPresent to return false
       controllerComponent.isControllerPresentMockValue = false;
       // reset so we don't think we've looked before
@@ -28,8 +26,6 @@ suite(controllerComponentName, function () {
       controllerComponent.checkIfControllerPresent();
       // check assertions
       assert.notOk(injectTrackedControlsSpy.called);
-      assert.notOk(addEventListenersSpy.called);
-      assert.ok(removeEventListenersSpy.called);
       assert.ok(controllerComponent.controllerPresent === false); // not undefined
     });
 
@@ -55,9 +51,7 @@ suite(controllerComponentName, function () {
     test('attach events if controller is newly present', function () {
       var el = this.el;
       var controllerComponent = el.components[controllerComponentName];
-      var addEventListenersSpy = this.sinon.spy(controllerComponent, 'addEventListeners');
       var injectTrackedControlsSpy = this.sinon.spy(controllerComponent, 'injectTrackedControls');
-      var removeEventListenersSpy = this.sinon.spy(controllerComponent, 'removeEventListeners');
       // mock isControllerPresent to return true
       controllerComponent.isControllerPresentMockValue = true;
       // reset so we don't think we've looked before
@@ -66,8 +60,6 @@ suite(controllerComponentName, function () {
       controllerComponent.checkIfControllerPresent();
       // check assertions
       assert.ok(injectTrackedControlsSpy.called);
-      assert.ok(addEventListenersSpy.called);
-      assert.notOk(removeEventListenersSpy.called);
       assert.ok(controllerComponent.controllerPresent);
     });
 
@@ -76,7 +68,6 @@ suite(controllerComponentName, function () {
       var controllerComponent = el.components[controllerComponentName];
       var addEventListenersSpy = this.sinon.spy(controllerComponent, 'addEventListeners');
       var injectTrackedControlsSpy = this.sinon.spy(controllerComponent, 'injectTrackedControls');
-      var removeEventListenersSpy = this.sinon.spy(controllerComponent, 'removeEventListeners');
       // mock isControllerPresent to return true
       controllerComponent.isControllerPresentMockValue = true;
       // pretend we've looked before
@@ -86,16 +77,13 @@ suite(controllerComponentName, function () {
       // check assertions
       assert.notOk(injectTrackedControlsSpy.called);
       assert.notOk(addEventListenersSpy.called);
-      assert.notOk(removeEventListenersSpy.called);
       assert.ok(controllerComponent.controllerPresent);
     });
 
     test('if controller disappears, remove event listeners', function () {
       var el = this.el;
       var controllerComponent = el.components[controllerComponentName];
-      var addEventListenersSpy = this.sinon.spy(controllerComponent, 'addEventListeners');
       var injectTrackedControlsSpy = this.sinon.spy(controllerComponent, 'injectTrackedControls');
-      var removeEventListenersSpy = this.sinon.spy(controllerComponent, 'removeEventListeners');
       // mock isControllerPresent to return true
       controllerComponent.isControllerPresentMockValue = false;
       // pretend we've looked before
@@ -104,8 +92,6 @@ suite(controllerComponentName, function () {
       controllerComponent.checkIfControllerPresent();
       // check assertions
       assert.notOk(injectTrackedControlsSpy.called);
-      assert.notOk(addEventListenersSpy.called);
-      assert.ok(removeEventListenersSpy.called);
       assert.notOk(controllerComponent.controllerPresent);
     });
   });
