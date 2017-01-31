df2f22dad8fa6272843d4cd1951e6af7ab78504e
Kevin <me@ngokevin.com>
2017-01-31 15:33:08 -0800

    ensure all components don't update if data has not changed (fixes #2322)

--- tests/utils/objects.test.js
+++ tests/utils/objects.test.js
@@ -61,6 +61,22 @@ suite('utils.objects', function () {
       assert.ok(deepEqual(objA, objB));
     });
 
+    test('can compare strings', function () {
+      var valA = 'abc';
+      var valB = 'abc';
+      var valC = 'def';
+      assert.ok(deepEqual(valA, valB));
+      assert.notOk(deepEqual(valA, valC));
+    });
+
+    test('can compare numbers', function () {
+      var valA = 1;
+      var valB = 1;
+      var valC = 2;
+      assert.ok(deepEqual(valA, valB));
+      assert.notOk(deepEqual(valA, valC));
+    });
+
     test('can compare for missing properties', function () {
       var objA = {id: 62, label: 'Foo', parent: null};
       var objB = {id: 62, label: 'Foo', parent: null, extraProp: true};
@@ -81,13 +97,30 @@ suite('utils.objects', function () {
       assert.notOk(deepEqual(objA, objC));
     });
 
-    /** SKIPPED: Comparison of nested objects is not yet implemented. */
-    test.skip('can compare nested objects', function () {
+    test('can compare nested objects', function () {
       var objA = {metadata: {source: 'Wikipedia, 2016'}};
       var objB = {metadata: {source: 'Wikipedia, 2016'}};
       var objC = {metadata: {source: 'Nature, 2015'}};
       assert.ok(deepEqual(objA, objB));
       assert.notOk(deepEqual(objA, objC));
+    });
+
+    test('can compare vec3s', function () {
+      var objA = {x: 0, y: 0, z: 0};
+      var objB = {x: 0, y: 0, z: 0};
+      var objC = {x: 1, y: 2, z: 3};
+      assert.ok(deepEqual(objA, objB));
+      assert.notOk(deepEqual(objA, objC));
+    });
+
+    test('can compare with null', function () {
+      assert.ok(deepEqual(null, null));
+      assert.notOk(deepEqual(null, {}));
+    });
+
+    test('can compare empty objects', function () {
+      assert.ok(deepEqual({}, {}));
+      assert.notOk(deepEqual({}, {a: 1}));
     });
   });
 });
