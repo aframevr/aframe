76dba27b3ced55c4d59a956565432f075a7a4016
Mark <qdmark@gmail.com>
2017-06-25 17:04:18 -0700

    lint minor

--- src/core/propertyTypes.js
+++ src/core/propertyTypes.js
@@ -123,7 +123,7 @@ function selectorParse (value) {
   if (!value) { return null; }
   if (typeof value !== 'string') { return value; }
   if (value[0] === '#' && !/[.,[: ]/.test(value)) {
-    //when selecting element by id only, use getElementById for better performance
+    // when selecting element by id only, use getElementById for better performance
     return document.getElementById(value.substring(1));
   }
   return document.querySelector(value);
