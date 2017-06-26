10bd9d8e1ed54f08debf8defa1f8783fd9010235
Mark <qdmark@gmail.com>
2017-06-26 12:52:08 -0700

    fix the regex check to see if the query is for selecting element by id only in queryParser

--- src/core/propertyTypes.js
+++ src/core/propertyTypes.js
@@ -122,7 +122,7 @@ function numberParse (value) {
 function selectorParse (value) {
   if (!value) { return null; }
   if (typeof value !== 'string') { return value; }
-  if (value[0] === '#' && !/[.,[: ]/.test(value)) {
+  if (value[0] === '#' && !/[,> ]/.test(value)) {
     // when selecting element by id only, use getElementById for better performance
     return document.getElementById(value.substring(1));
   }
