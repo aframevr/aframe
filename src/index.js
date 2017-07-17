ce770fd3f56506b750a23ce405b0ea5b3a74e975
a-frobot <aframebot@gmail.com>
2017-07-17 22:50:33 +0000

    Bump aframe-master dist/ builds. (https://github.com/aframevr/aframe/compare/8cd9b050ae2e...f5c17e4d70ca)

--- src/index.js
+++ src/index.js
@@ -83,7 +83,7 @@ require('./core/a-mixin');
 require('./extras/components/');
 require('./extras/primitives/');
 
-console.log('A-Frame Version: 0.6.0 (Date 17-07-2017, Commit #2a54909)');
+console.log('A-Frame Version: 0.6.0 (Date 17-07-2017, Commit #f5c17e4)');
 console.log('three Version:', pkg.dependencies['three']);
 console.log('WebVR Polyfill Version:', pkg.dependencies['webvr-polyfill']);
 
