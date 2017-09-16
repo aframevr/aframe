854b63b736e2caca30e8451a8a72e2636de8aef7
Diego Marcos <diego.marcos@gmail.com>
2017-09-16 15:05:48 -0700

    Make sure that raycaster is set when controllermodelready does not fire (#3060)
    (it currently only fires with windows-motion-controls)

--- src/components/laser-controls.js
+++ src/components/laser-controls.js
@@ -10,6 +10,7 @@ registerComponent('laser-controls', {
     var config = this.config;
     var data = this.data;
     var el = this.el;
+    var self = this;
 
     // Set all controller models.
     el.setAttribute('daydream-controls', {hand: data.hand});
@@ -20,7 +21,10 @@ registerComponent('laser-controls', {
 
     // Wait for controller to connect, or have a valid pointing pose, before creating ray
     el.addEventListener('controllerconnected', createRay);
-    el.addEventListener('controllermodelready', createRay);
+    el.addEventListener('controllermodelready', function (evt) {
+      createRay(evt);
+      self.modelReady = true;
+    });
 
     function createRay (evt) {
       var controllerConfig = config[evt.detail.name];
@@ -43,7 +47,7 @@ registerComponent('laser-controls', {
 
       // Only apply a default raycaster if it does not yet exist. This prevents it overwriting
       // config applied from a controllermodelready event.
-      if (evt.detail.rayOrigin || !el.hasAttribute('raycaster')) {
+      if (evt.detail.rayOrigin || !self.modelReady) {
         el.setAttribute('raycaster', raycasterConfig);
       }
 
