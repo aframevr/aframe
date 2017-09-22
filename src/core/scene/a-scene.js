75ac4d841e57b4a494f46e2ca7dbac4c08facf3b
Olga Milovanova <31327535+olga-microsoft@users.noreply.github.com>
2017-09-22 10:09:27 -0700

    Do not resize the canvas while presenting or on mobile (#3080)

--- src/core/scene/a-scene.js
+++ src/core/scene/a-scene.js
@@ -456,11 +456,13 @@ module.exports.AScene = registerElement('a-scene', {
         var canvas = this.canvas;
         var embedded = this.getAttribute('embedded') && !this.is('vr-mode');
         var size;
+        var isEffectPresenting = this.effect && this.effect.isPresenting;
         // Do not update renderer, if a camera or a canvas have not been injected.
         // In VR mode, VREffect handles canvas resize based on the dimensions returned by
         // the getEyeParameters function of the WebVR API. These dimensions are independent of
-        // the window size, therefore should not be overwritten with the window's width and height.
-        if (!camera || !canvas || this.is('vr-mode')) { return; }
+        // the window size, therefore should not be overwritten with the window's width and height,
+        // except when in fullscreen mode.
+        if (!camera || !canvas || (this.is('vr-mode') && (this.isMobile || isEffectPresenting))) { return; }
         // Update camera.
         size = getCanvasSize(canvas, embedded);
         camera.aspect = size.width / size.height;
