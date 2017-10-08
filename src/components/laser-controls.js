efe152352a54978f11d4f103e56659cf7d3c67aa
William Murphy <william@datatitian.com>
2017-10-08 18:01:21 -0500

    Move laser-controls init helper functions into component properties so they can be referenced later to remove event listeners. Remove event listeners when laser-controls is removed

--- src/components/laser-controls.js
+++ src/components/laser-controls.js
@@ -1,5 +1,6 @@
 var registerComponent = require('../core/component').registerComponent;
 var utils = require('../utils/');
+var bind = utils.bind;
 
 registerComponent('laser-controls', {
   schema: {
@@ -7,10 +8,8 @@ registerComponent('laser-controls', {
   },
 
   init: function () {
-    var config = this.config;
     var data = this.data;
     var el = this.el;
-    var self = this;
 
     // Set all controller models.
     el.setAttribute('daydream-controls', {hand: data.hand});
@@ -19,51 +18,60 @@ registerComponent('laser-controls', {
     el.setAttribute('vive-controls', {hand: data.hand});
     el.setAttribute('windows-motion-controls', {hand: data.hand});
 
+    // Bind methods
+    this.createRay = bind(this.createRay, this);
+    this.hideRay = bind(this.hideRay, this);
+    this.setModelReady = bind(this.setModelReady, this);
+
     // Wait for controller to connect, or have a valid pointing pose, before creating ray
-    el.addEventListener('controllerconnected', createRay);
-    el.addEventListener('controllerdisconnected', hideRay);
-    el.addEventListener('controllermodelready', function (evt) {
-      createRay(evt);
-      self.modelReady = true;
-    });
-
-    function createRay (evt) {
-      var controllerConfig = config[evt.detail.name];
-
-      if (!controllerConfig) { return; }
-
-      // Show the line unless a particular config opts to hide it, until a controllermodelready
-      // event comes through.
-      var raycasterConfig = utils.extend({
-        showLine: true
-      }, controllerConfig.raycaster || {});
-
-      // The controllermodelready event contains a rayOrigin that takes into account
-      // offsets specific to the loaded model.
-      if (evt.detail.rayOrigin) {
-        raycasterConfig.origin = evt.detail.rayOrigin.origin;
-        raycasterConfig.direction = evt.detail.rayOrigin.direction;
-        raycasterConfig.showLine = true;
-      }
-
-      // Only apply a default raycaster if it does not yet exist. This prevents it overwriting
-      // config applied from a controllermodelready event.
-      if (evt.detail.rayOrigin || !self.modelReady) {
-        el.setAttribute('raycaster', raycasterConfig);
-      } else {
-        el.setAttribute('raycaster', 'showLine', true);
-      }
-
-      el.setAttribute('cursor', utils.extend({
-        fuse: false
-      }, controllerConfig.cursor));
-    }
-
-    function hideRay () {
-      el.setAttribute('raycaster', 'showLine', false);
-    }
+    el.addEventListener('controllerconnected', this.createRay);
+    el.addEventListener('controllerdisconnected', this.hideRay);
+    el.addEventListener('controllermodelready', this.createRay);
+    el.addEventListener('controllermodelready', this.setModelReady);
   },
+  remove: function () {
+    this.el.removeEventListener('controllerconnected', this.createRay);
+    this.el.removeEventListener('controllerdisconnected', this.hideRay);
+    this.el.removeEventListener('controllermodelready', this.createRay);
+    this.el.removeEventListener('controllermodelready', this.setModelReady);
+  },
+  createRay: function (evt) {
+    var controllerConfig = this.config[evt.detail.name];
 
+    if (!controllerConfig) { return; }
+
+    // Show the line unless a particular config opts to hide it, until a controllermodelready
+    // event comes through.
+    var raycasterConfig = utils.extend({
+      showLine: true
+    }, controllerConfig.raycaster || {});
+
+    // The controllermodelready event contains a rayOrigin that takes into account
+    // offsets specific to the loaded model.
+    if (evt.detail.rayOrigin) {
+      raycasterConfig.origin = evt.detail.rayOrigin.origin;
+      raycasterConfig.direction = evt.detail.rayOrigin.direction;
+      raycasterConfig.showLine = true;
+    }
+
+    // Only apply a default raycaster if it does not yet exist. This prevents it overwriting
+    // config applied from a controllermodelready event.
+    if (evt.detail.rayOrigin || !this.modelReady) {
+      this.el.setAttribute('raycaster', raycasterConfig);
+    } else {
+      this.el.setAttribute('raycaster', 'showLine', true);
+    }
+
+    this.el.setAttribute('cursor', utils.extend({
+      fuse: false
+    }, controllerConfig.cursor));
+  },
+  hideRay: function () {
+    this.el.setAttribute('raycaster', 'showLine', false);
+  },
+  setModelReady: function () {
+    this.modelReady = true;
+  },
   config: {
     'daydream-controls': {
       cursor: {downEvents: ['trackpaddown'], upEvents: ['trackpadup']}
