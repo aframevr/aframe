bfacbdc69b5c2a3355557658003efd420a219ed9
a-frobot <aframebot@gmail.com>
2017-11-19 08:09:48 +0000

    Bump aframe-master dist/ builds. (https://github.com/aframevr/aframe/compare/fbb94296ee59...cd58a74605da)

--- dist/aframe-master.js
+++ dist/aframe-master.js
@@ -77442,15 +77442,6 @@ registerPrimitive('a-camera', {
     'reverse-mouse-drag': 'look-controls.reverseMouseDrag',
     'user-height': 'camera.userHeight',
     zoom: 'camera.zoom'
-  },
-
-  deprecatedMappings: {
-    'cursor-color': 'a-camera[cursor-color] has been removed. Use a-cursor[color] instead.',
-    'cursor-maxdistance': 'a-camera[cursor-maxdistance] has been removed. Use a-cursor[max-distance] instead.',
-    'cursor-offset': 'a-camera[cursor-offset] has been removed. Use a-cursor[position] instead.',
-    'cursor-opacity': 'a-camera[cursor-offset] has been removed. Use a-cursor[opacity] instead.',
-    'cursor-scale': 'a-camera[cursor-scale] has been removed. Use a-cursor[scale] instead.',
-    'cursor-visible': 'a-camera[cursor-visible] has been removed. Use a-cursor[visible] instead.'
   }
 });
 
@@ -78172,7 +78163,7 @@ _dereq_('./core/a-mixin');
 _dereq_('./extras/components/');
 _dereq_('./extras/primitives/');
 
-console.log('A-Frame Version: 0.7.0 (Date 2017-11-17, Commit #148b4a4)');
+console.log('A-Frame Version: 0.7.0 (Date 2017-11-19, Commit #cd58a74)');
 console.log('three Version:', pkg.dependencies['three']);
 console.log('WebVR Polyfill Version:', pkg.dependencies['webvr-polyfill']);
 
