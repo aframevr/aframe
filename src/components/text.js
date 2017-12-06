01f6bcf1869f9c027350212aeca3b2262975d97d
Kevin Ngo <me@ngokevin.com>
2017-12-05 17:54:05 -0800

    add xOffset option to allow text padding (#3269)

--- src/components/text.js
+++ src/components/text.js
@@ -76,6 +76,8 @@ module.exports.Component = registerComponent('text', {
     wrapCount: {type: 'number', default: 40},
     // `wrapPixels` will wrap using bmfont pixel units (e.g., dejavu's is 32 pixels).
     wrapPixels: {type: 'number'},
+    // `xOffset` to add padding.
+    xOffset: {type: 'number', default: 0},
     // `yOffset` to adjust generated fonts from tools that may have incorrect metrics.
     yOffset: {type: 'number', default: 0},
     // `zOffset` will provide a small z offset to avoid z-fighting.
@@ -313,7 +315,7 @@ module.exports.Component = registerComponent('text', {
     }
 
     // Position and scale mesh to apply layout.
-    mesh.position.x = x * textScale;
+    mesh.position.x = x * textScale + data.xOffset;
     mesh.position.y = y * textScale;
     // Place text slightly in front to avoid Z-fighting.
     mesh.position.z = data.zOffset;
