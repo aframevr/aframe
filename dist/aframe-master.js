9fe682405874c61e8320c74f9876461b82fc9fa3
a-frobot <aframebot@gmail.com>
2017-12-15 22:53:29 +0000

    Bump aframe-master dist/ builds. (https://github.com/aframevr/aframe/compare/1bee33240399...16540abda062)

--- dist/aframe-master.js
+++ dist/aframe-master.js
@@ -77430,7 +77430,7 @@ _dereq_('./core/a-mixin');
 _dereq_('./extras/components/');
 _dereq_('./extras/primitives/');
 
-console.log('A-Frame Version: 0.7.0 (Date 2017-12-14, Commit #e382969)');
+console.log('A-Frame Version: 0.7.0 (Date 2017-12-15, Commit #16540ab)');
 console.log('three Version:', pkg.dependencies['three']);
 console.log('WebVR Polyfill Version:', pkg.dependencies['webvr-polyfill']);
 
@@ -79244,12 +79244,21 @@ module.exports.PolyfillControls = function PolyfillControls (object) {
  * Split a delimited component property string (e.g., `material.color`) to an object
  * containing `component` name and `property` name. If there is no delimiter, just return the
  * string back.
+ *
+ * Cache arrays from splitting strings via delimiter to save on memory.
+ *
+ * @param {string} str - e.g., `material.opacity`.
+ * @param {string} delimiter - e.g., `.`.
+ * @returns {array} e.g., `['material', 'opacity']`.
  */
-module.exports.getComponentPropertyPath = function (str, delimiter) {
-  delimiter = delimiter || '.';
-  if (str.indexOf(delimiter) === -1) { return str; }
-  return str.split(delimiter);
-};
+var propertyPathCache = {};
+function getComponentPropertyPath (str, delimiter) {
+  if (!propertyPathCache[delimiter]) { propertyPathCache[delimiter] = {}; }
+  propertyPathCache[delimiter][str] = str.split(delimiter);
+  return propertyPathCache[delimiter][str];
+}
+module.exports.getComponentPropertyPath = getComponentPropertyPath;
+module.exports.propertyPathCache = propertyPathCache;
 
 /**
  * Get component property using encoded component name + component property name with a
@@ -79259,7 +79268,7 @@ module.exports.getComponentProperty = function (el, name, delimiter) {
   var splitName;
   delimiter = delimiter || '.';
   if (name.indexOf(delimiter) !== -1) {
-    splitName = name.split(delimiter);
+    splitName = getComponentPropertyPath(name, delimiter);
     return el.getAttribute(splitName[0])[splitName[1]];
   }
   return el.getAttribute(name);
@@ -79274,7 +79283,7 @@ module.exports.setComponentProperty = function (el, name, value, delimiter) {
   var splitName;
   delimiter = delimiter || '.';
   if (name.indexOf(delimiter) !== -1) {
-    splitName = name.split(delimiter);
+    splitName = getComponentPropertyPath(name, delimiter);
     data[splitName[1]] = value;
     el.setAttribute(splitName[0], data);
     return;
