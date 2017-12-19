6bf22fdfddd58eaff57d6ad436dc4ee5e91b4c0a
Kevin Ngo <me@ngokevin.com>
2017-12-19 15:36:18 -0800

    get rid of some string split operations

--- src/core/a-assets.js
+++ src/core/a-assets.js
@@ -234,7 +234,7 @@ function extractDomain (url) {
   var domain = url.indexOf('://') > -1 ? url.split('/')[2] : url.split('/')[0];
 
   // Find and remove port number.
-  return domain.split(':')[0];
+  return domain.substring(0, domain.indexOf(':'));
 }
 
 /**
