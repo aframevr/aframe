d7a45dc241588730289c897b988eb1436a94127d
dmarcos <diego.marcos@gmail.com>
2017-12-21 06:01:15 -0800

    Add transparent flag to be able to define transparent backgrounds

--- src/components/scene/background.js
+++ src/components/scene/background.js
@@ -2,8 +2,17 @@
 var register = require('../../core/component').registerComponent;
 
 module.exports.Component = register('background', {
-  schema: {color: {type: 'color', default: 'black'}},
+  schema: {
+    color: {type: 'color', default: 'black'},
+    transparent: {default: false}
+  },
   update: function () {
-    this.el.object3D.background = new THREE.Color(this.data.color);
+    var data = this.data;
+    var object3D = this.el.object3D;
+    if (data.transparent) {
+      object3D.background = null;
+      return;
+    }
+    object3D.background = new THREE.Color(data.color);
   }
 });
