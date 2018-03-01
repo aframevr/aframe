e45ff72bfd4186e4f5e97ef6dc7ec8c993fb1429
Kevin Ngo <me@ngokevin.com>
2018-03-01 15:40:55 -0800

    Revert "fix black texture issue with a-sky and a-videosphere components with three r89"
    This reverts commit 660463e0b853063b2afd32a101b711bef02c34b4.

--- src/extras/primitives/primitives/a-sky.js
+++ src/extras/primitives/primitives/a-sky.js
@@ -14,7 +14,6 @@ registerPrimitive('a-sky', utils.extendDeep({}, getMeshMixin(), {
     material: {
       color: '#FFF',
       shader: 'flat',
-      side: 'back',
       npot: true
     },
     scale: '-1 1 1'
--- src/extras/primitives/primitives/a-videosphere.js
+++ src/extras/primitives/primitives/a-videosphere.js
@@ -13,7 +13,6 @@ registerPrimitive('a-videosphere', utils.extendDeep({}, getMeshMixin(), {
     material: {
       color: '#FFF',
       shader: 'flat',
-      side: 'back',
       npot: true
     },
     scale: '-1 1 1'
