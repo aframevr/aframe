10259badf253a4cb110ef5e8cb74afb0963c6eef
a-frobot <aframebot@gmail.com>
2018-03-22 18:09:40 +0000

    Bump aframe-master dist/ builds. (https://github.com/aframevr/aframe/compare/6c67b2551c91...f6c04f9a4627)

--- dist/aframe-master.js
+++ dist/aframe-master.js
@@ -73003,10 +73003,15 @@ module.exports.AScene = registerElement('a-scene', {
         this.isMobile = isMobile;
         this.isScene = true;
         this.object3D = new THREE.Scene();
+        var self = this;
+        this.object3D.onAfterRender = function (renderer, scene, camera) {
+          // THREE may swap the camera used for the rendering if in VR, so we pass it to tock
+          if (self.isPlaying) { self.tock(self.time, self.delta, camera); }
+        };
         this.render = bind(this.render, this);
         this.systems = {};
         this.systemNames = [];
-        this.time = 0;
+        this.time = this.delta = 0;
         this.init();
       }
     },
@@ -73545,20 +73550,20 @@ module.exports.AScene = registerElement('a-scene', {
      * needing to render.
      */
     tock: {
-      value: function (time, timeDelta) {
+      value: function (time, timeDelta, camera) {
         var i;
         var systems = this.systems;
 
         // Components.
         for (i = 0; i < this.behaviors.tock.length; i++) {
           if (!this.behaviors.tock[i].el.isPlaying) { continue; }
-          this.behaviors.tock[i].tock(time, timeDelta);
+          this.behaviors.tock[i].tock(time, timeDelta, camera);
         }
 
         // Systems.
         for (i = 0; i < this.systemNames.length; i++) {
           if (!systems[this.systemNames[i]].tock) { continue; }
-          systems[this.systemNames[i]].tock(time, timeDelta);
+          systems[this.systemNames[i]].tock(time, timeDelta, camera);
         }
       }
     },
@@ -73572,16 +73577,14 @@ module.exports.AScene = registerElement('a-scene', {
      */
     render: {
       value: function () {
-        var delta = this.clock.getDelta() * 1000;
+        this.delta = this.clock.getDelta() * 1000;
         var renderer = this.renderer;
         this.time = this.clock.elapsedTime * 1000;
 
-        if (this.isPlaying) { this.tick(this.time, delta); }
+        if (this.isPlaying) { this.tick(this.time, this.delta); }
 
         renderer.animate(this.render);
         renderer.render(this.object3D, this.camera, this.renderTarget);
-
-        if (this.isPlaying) { this.tock(this.time, delta); }
       },
       writable: true
     }
@@ -75395,7 +75398,7 @@ _dereq_('./core/a-mixin');
 _dereq_('./extras/components/');
 _dereq_('./extras/primitives/');
 
-console.log('A-Frame Version: 0.8.1 (Date 2018-03-19, Commit #56e86d6)');
+console.log('A-Frame Version: 0.8.1 (Date 2018-03-22, Commit #063dd8c)');
 console.log('three Version:', pkg.dependencies['three']);
 console.log('WebVR Polyfill Version:', pkg.dependencies['webvr-polyfill']);
 
--- src/index.js
+++ src/index.js
@@ -80,7 +80,7 @@ require('./core/a-mixin');
 require('./extras/components/');
 require('./extras/primitives/');
 
-console.log('A-Frame Version: 0.8.1 (Date 2018-03-19, Commit #56e86d6)');
+console.log('A-Frame Version: 0.8.1 (Date 2018-03-22, Commit #063dd8c)');
 console.log('three Version:', pkg.dependencies['three']);
 console.log('WebVR Polyfill Version:', pkg.dependencies['webvr-polyfill']);
 
