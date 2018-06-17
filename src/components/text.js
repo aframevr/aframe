2d614b04039e5d842d4a7108fdeb1ef822acfc97
Don McCurdy <donmccurdy@google.com>
2018-06-17 21:50:41 +0200

    [text] Read URL of first texture page from JSON.

--- src/components/text.js
+++ src/components/text.js
@@ -36,6 +36,9 @@ var cache = new PromiseCache();
 var fontWidthFactors = {};
 var textures = {};
 
+// Regular expression for detecting a URLs with a protocol prefix.
+var protocolRe = /^\w+:/;
+
 /**
  * SDF-based text component.
  * Based on https://github.com/Jam3/three-bmfont-text.
@@ -95,13 +98,12 @@ module.exports.Component = registerComponent('text', {
   update: function (oldData) {
     var data = coerceData(this.data);
     var font = this.currentFont;
-    var fontImage = this.getFontImageSrc();
 
-    if (textures[fontImage]) {
-      this.texture = textures[fontImage];
+    if (textures[data.font]) {
+      this.texture = textures[data.font];
     } else {
       // Create texture per font.
-      this.texture = textures[fontImage] = new THREE.Texture();
+      this.texture = textures[data.font] = new THREE.Texture();
       this.texture.anisotropy = MAX_ANISOTROPY;
     }
 
@@ -234,7 +236,7 @@ module.exports.Component = registerComponent('text', {
         var texture = self.texture;
         texture.image = image;
         texture.needsUpdate = true;
-        textures[fontImgSrc] = texture;
+        textures[data.font] = texture;
         self.texture = texture;
         self.mesh.visible = true;
         el.emit('textfontset', {font: data.font, fontObj: font});
@@ -249,8 +251,15 @@ module.exports.Component = registerComponent('text', {
   },
 
   getFontImageSrc: function () {
+    if (this.data.fontImage) { return this.data.fontImage; }
     var fontSrc = this.lookupFont(this.data.font || DEFAULT_FONT) || this.data.font;
-    return this.data.fontImage || fontSrc.replace(/(\.fnt)|(\.json)/, '.png');
+    var imageSrc = this.currentFont.pages[0];
+    // If the image URL contains a non-HTTP(S) protocol, assume it's an absolute
+    // path on disk and try to infer the path from the font source instead.
+    if (imageSrc.match(protocolRe) && imageSrc.indexOf('http') !== 0) {
+      return fontSrc.replace(/(\.fnt)|(\.json)/, '.png');
+    }
+    return THREE.LoaderUtils.extractUrlBase(fontSrc) + imageSrc;
   },
 
   /**
