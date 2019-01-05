16b0753eb01046abdc7a764a7c7268b27ff3c77b
obrienluk89@gmail.com <monkeyPot645>
2019-01-05 18:19:38 +0000

    Linkting arggh!

--- src/utils/autoPropertyMapping.js
+++ src/utils/autoPropertyMapping.js
@@ -1,17 +1,17 @@
-var components = require('../core/component').components
+var components = require('../core/component').components;
 
-function camelCaseToDash( myStr ) {
-    return myStr.replace( /([a-z])([A-Z])/g, '$1-$2' ).toLowerCase();
+function camelCaseToDash (myStr) {
+  return myStr.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
 }
 
-module.exports.autoPropertyMapping = function(componentName) {
-    let mappings = {};
+module.exports.autoPropertyMapping = function (componentName) {
+  let mappings = {};
 
-    if (components[componentName]) {
-        Object.keys(components[componentName].schema).forEach(function (key, index) {
-            mappings[camelCaseToDash(key)] = componentName + '.' + key
-        });
-    }
+  if (components[componentName]) {
+    Object.keys(components[componentName].schema).forEach(function (key, index) {
+      mappings[camelCaseToDash(key)] = componentName + '.' + key;
+    });
+  }
 
-    return mappings;
-}
+  return mappings;
+};
