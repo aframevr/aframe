6c916c87f7ecd4b5aa712ace8469c64fbf717aba
Will Murphy <william@3data.io>
2019-02-14 09:31:55 -0600

    remove properties added to objectPool objects by dynamic schemas in their prior use

--- src/utils/object-pool.js
+++ src/utils/object-pool.js
@@ -76,3 +76,14 @@ function clearObject (obj) {
   for (key in obj) { obj[key] = undefined; }
 }
 module.exports.clearObject = clearObject;
+
+function removeUnusedKeys (obj, schema) {
+  var key;
+  if (!obj || obj.constructor !== Object) { return; }
+  for (key in obj) {
+    if (!(key in schema)) {
+      delete obj[key];
+    }
+  }
+}
+module.exports.removeUnusedKeys = removeUnusedKeys;
