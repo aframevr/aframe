2b910f029eab493fcdc902736bbb00ed71909268
Will Murphy <40799701+3DataWill@users.noreply.github.com>
2019-02-14 10:10:01 -0600

    Fix coordinates stringify for zeroed coordinates (#4017)
    * add test case for stringification of zeroed coordinates
    
    * change from truthy check to null/undefined check when determining coordinate dimensionality

--- src/utils/coordinates.js
+++ src/utils/coordinates.js
@@ -76,8 +76,8 @@ function stringify (data) {
   var str;
   if (typeof data !== OBJECT) { return data; }
   str = data.x + ' ' + data.y;
-  if (data.z) { str += ' ' + data.z; }
-  if (data.w) { str += ' ' + data.w; }
+  if (data.z != null) { str += ' ' + data.z; }
+  if (data.w != null) { str += ' ' + data.w; }
   return str;
 }
 module.exports.stringify = stringify;
