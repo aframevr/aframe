76eb7ebafa8f1ccd8f7e71487a645e1cd1fc9141
Kevin Ngo <me@ngokevin.com>
2019-02-15 17:12:03 -0800

    Revert "utils.coordinates change != to !=="
    This reverts commit 990d20f21cb01cb511e11edce39b9df3a1ee78c8.

--- src/utils/coordinates.js
+++ src/utils/coordinates.js
@@ -76,8 +76,8 @@ function stringify (data) {
   var str;
   if (typeof data !== OBJECT) { return data; }
   str = data.x + ' ' + data.y;
-  if (data.z !== null) { str += ' ' + data.z; }
-  if (data.w !== null) { str += ' ' + data.w; }
+  if (data.z != null) { str += ' ' + data.z; }
+  if (data.w != null) { str += ' ' + data.w; }
   return str;
 }
 module.exports.stringify = stringify;
