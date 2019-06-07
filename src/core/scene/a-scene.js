5edbc0956014035f73bbe4bd6074b3edb5f4eefc
Artem Bolgar <Artem.Bolgar@oculus.com>
2019-06-06 19:22:44 -0700

    One more feedback from Diego.

--- src/core/scene/a-scene.js
+++ src/core/scene/a-scene.js
@@ -314,11 +314,7 @@ module.exports.AScene = registerElement('a-scene', {
           // to setup everything from there. Thus, we need to emulate another vrdisplaypresentchange
           // for the actual requestPresent. Need to make sure there are no issues with firing the
           // vrdisplaypresentchange multiple times.
-          var event = new CustomEvent('vrdisplaypresentchange', {
-            detail: {
-              display: utils.device.getVRDisplay()
-            }
-          });
+          var event = new CustomEvent('vrdisplaypresentchange', {detail: {display: utils.device.getVRDisplay()}});
           window.dispatchEvent(event);
 
           self.addState('vr-mode');
