66827ccdb4f03219d7c649a9aeab631813a93506
Edward Silverton <e.silverton@gmail.com>
2019-08-08 04:36:27 +0100

    only emit cursor mouseup if mousedown originated on scene (#4249)
    * only emit mouseup if cursor is down on scene
    
    * Style nit
    
    * Fix tests

--- tests/components/cursor.test.js
+++ tests/components/cursor.test.js
@@ -123,6 +123,7 @@ suite('cursor', function () {
       once(el, 'mouseup', function () {
         done();
       });
+      component.isCursorDown = true;
       component.onCursorUp();
     });
 
@@ -133,6 +134,7 @@ suite('cursor', function () {
       once(intersectedEl, 'mouseup', function () {
         done();
       });
+      component.isCursorDown = true;
       component.onCursorUp();
     });
 
@@ -143,6 +145,7 @@ suite('cursor', function () {
       once(el, 'click', function () {
         done();
       });
+      component.isCursorDown = true;
       component.onCursorUp();
     });
 
@@ -153,6 +156,7 @@ suite('cursor', function () {
       once(intersectedEl, 'click', function () {
         done();
       });
+      component.isCursorDown = true;
       component.onCursorUp();
     });
 
@@ -163,6 +167,7 @@ suite('cursor', function () {
       component.intersectedEl = intersectedEl;
       component.cursorDownEl = intersectedEl;
       once(intersectedEl, 'click', function () { done(); });
+      component.isCursorDown = true;
       component.onCursorUp({type: 'touchend', preventDefault: function () {}});
     });
   });
