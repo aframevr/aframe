ad8944efc401baf86cb834700a4c82f73a1a5db0
Diego Marcos <diego.marcos@gmail.com>
2020-01-17 15:43:02 -0800

    Request / Reset referenceSpace when entering / exiting VR (fix #4406)

--- src/systems/tracked-controls-webxr.js
+++ src/systems/tracked-controls-webxr.js
@@ -10,15 +10,34 @@ module.exports.System = registerSystem('tracked-controls-webxr', {
     this.controllers = [];
     this.oldControllersLength = 0;
     this.throttledUpdateControllerList = utils.throttle(this.updateControllerList, 500, this);
+    this.updateReferenceSpace = this.updateReferenceSpace.bind(this);
+    this.el.addEventListener('enter-vr', this.updateReferenceSpace);
+    this.el.addEventListener('exit-vr', this.updateReferenceSpace);
   },
 
   tick: function () {
     this.throttledUpdateControllerList();
   },
 
+  updateReferenceSpace: function () {
+    var self = this;
+    var xrSession = this.el.xrSession;
+    if (!xrSession) {
+      this.referenceSpace = undefined;
+      this.controllers = [];
+      if (this.oldControllersLength > 0) {
+        this.oldControllersLength = 0;
+        this.el.emit('controllersupdated', undefined, false);
+      }
+      return;
+    }
+    xrSession.requestReferenceSpace('local-floor').then(function (referenceSpace) {
+      self.referenceSpace = referenceSpace;
+    });
+  },
+
   updateControllerList: function () {
     var xrSession = this.el.xrSession;
-    var self = this;
     if (!xrSession) {
       if (this.oldControllersLength === 0) { return; }
       // Broadcast that we now have zero controllers connected if there is
@@ -28,12 +47,9 @@ module.exports.System = registerSystem('tracked-controls-webxr', {
       this.el.emit('controllersupdated', undefined, false);
       return;
     }
-    this.controllers = this.el.xrSession.inputSources;
+    this.controllers = xrSession.inputSources;
     if (this.oldControllersLength === this.controllers.length) { return; }
     this.oldControllersLength = this.controllers.length;
-    xrSession.requestReferenceSpace('local-floor').then(function (referenceSpace) {
-      self.referenceSpace = referenceSpace;
-    });
     this.el.emit('controllersupdated', undefined, false);
   }
 });
