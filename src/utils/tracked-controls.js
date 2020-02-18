3dced3d4114f740478d4fffa5c222bd9200aacaf
atarng-magicleap <atarng@magicleap.com>
2020-02-18 18:18:31 -0500

    Gracefully handle lack of profiles from Controller (old definition in Mojom)

--- src/utils/tracked-controls.js
+++ src/utils/tracked-controls.js
@@ -162,7 +162,7 @@ function findMatchingControllerWebXR (controllers, idPrefix, handedness, index, 
   for (i = 0; i < controllers.length; i++) {
     controller = controllers[i];
     profiles = controller.profiles;
-    if (profiles.length === 0) { return; }
+    if (!profiles || profiles.length === 0) { continue; }
     if (iterateProfiles) {
       for (j = 0; j < profiles.length; j++) {
         controllerMatch = profiles[j].startsWith(idPrefix);
