335ae83d1f6c94bb1aed14dce44c57eb11836b68
Diego Marcos <diego.marcos@gmail.com>
2020-09-29 20:10:34 -0700

    Start cubemap layer support

--- src/components/layer.js
+++ src/components/layer.js
@@ -5,10 +5,8 @@ var warn = utils.debug('components:layer:warn');
 
 module.exports.Component = registerComponent('layer', {
   schema: {
-    updatesEveryFrame: {default: false},
-    width: {default: 800},
-    height: {default: 800},
-    src: {type: 'map'}
+    src: {type: 'map'},
+    type: {default: 'stereocubemap', oneOf: ['quad', 'monocubemap', 'stereocubemap']}
   },
 
   init: function () {
@@ -24,58 +22,203 @@ module.exports.Component = registerComponent('layer', {
     requiredFeatures.push('layers');
     this.el.sceneEl.getAttribute('webxr', 'requiredFeatures', requiredFeatures);
     this.el.sceneEl.addEventListener('enter-vr', this.onEnterVR);
+    this.el.sceneEl.addEventListener('exit-vr', this.onExitVR);
+    initQuad(gl);
   },
 
   bindMethods: function () {
     this.onRequestedReferenceSpace = this.onRequestedReferenceSpace.bind(this);
     this.onEnterVR = this.onEnterVR.bind(this);
+    this.onExitVR = this.onExitVR.bind(this);
   },
 
   update: function (oldData) {
-    if (this.data.src !== oldData.src) {
-      this.updateSrc();
-    }
+    if (this.data.src !== oldData.src) { this.updateSrc(); }
   },
 
   updateSrc: function () {
+    var type = this.data.type;
+    this.texture = undefined;
+    if (type === 'quad') {
+      this.loadQuadImage();
+      return;
+    }
+
+    if (type === 'monocubemap' || type === 'stereocubemap') {
+      this.loadCubeMapImages();
+      return;
+    }
+  },
+
+  loadCubeMapImages: function () {
+    var type = this.data.type;
+    var cubeFaceSize = this.cubeFaceSize;
+    var glayer;
+    var xrGLFactory = this.xrGLFactory;
+    var frame = this.el.sceneEl.frame;
+
+    if (!this.layer) { return; }
+    if (type === 'monocubemap') {
+      glayer = xrGLFactory.getSubImage(this.layer, frame);
+      this.loadCubeMapImage(glayer.colorTexture, cubeFaceSize, 0);
+    } else {
+      glayer = xrGLFactory.getSubImage(this.layer, frame, 'left');
+      this.loadCubeMapImage(glayer.colorTexture, cubeFaceSize, 0);
+      glayer = xrGLFactory.getSubImage(this.layer, frame, 'right');
+      this.loadCubeMapImage(glayer.colorTexture, cubeFaceSize, 6);
+    }
+  },
+
+  loadQuadImage: function () {
     var src = this.data.src;
     var self = this;
     this.el.sceneEl.systems.material.loadTexture(src, {src: src}, function textureLoaded (texture) {
-      var gl = self.el.sceneEl.renderer.getContext();
       self.el.sceneEl.renderer.initTexture(texture);
       self.texture = texture;
-      initQuad(gl, texture.image.width, texture.image.height);
+      if (src.tagName === 'VIDEO') { setTimeout(function () { self.textureIsVideo = true; }, 1000); }
+      if (self.layer) {
+        self.layer.height = self.texture.image.height;
+        self.layer.width = self.texture.image.width;
+        self.needsRedraw = true;
+      }
+      self.updateQuadPanel();
     });
   },
 
+  loadCubeMapImage: function (layerColorTexture, cubeFaceSize, src, faceOffset) {
+    var gl = this.el.sceneEl.renderer.getContext();
+    var textureSourceCubeFaceSize = Math.min(src.width, src.height);
+
+    // dont flip the pixels as we load them into the texture buffer.
+    // TEXTURE_CUBE_MAP expects the Y to be flipped for the faces and it already
+    // is flipped in our texture image.
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, false);
+    gl.bindTexture(gl.TEXTURE_CUBE_MAP, layerColorTexture);
+
+    var cubefaceTextures = [];
+    for (var i = 0; i < 6; i++) {
+      var tempCanvas = document.createElement('CANVAS');
+      tempCanvas.width = tempCanvas.height = cubeFaceSize;
+      var tempCanvasContext = tempCanvas.getContext('2d');
+      // Note that this call to drawImage will not only copy the bytes to the
+      // canvas but also could resized the image if our cube face size is
+      // smaller than the source image due to GL max texture size.
+      tempCanvasContext.drawImage(
+        layerColorTexture,
+        (i + faceOffset) * textureSourceCubeFaceSize, // top left x coord in source
+        0, // top left y coord in source
+        textureSourceCubeFaceSize, // x pixel count from source
+        textureSourceCubeFaceSize, // y pixel count from source
+        0, // dest x offset in the canvas
+        0, // dest y offset in the canvas
+        cubeFaceSize, // x pixel count in dest
+        cubeFaceSize, // y pixel count in dest
+      );
+      cubefaceTextures.push(tempCanvas);
+    }
+
+    var errorCode = 0;
+    cubefaceTextures.forEach(function (canvas, i) {
+      gl.texSubImage2D(
+        gl.TEXTURE_CUBE_MAP_POSITIVE_X + i,
+        0,
+        0, 0,
+        gl.RGBA,
+        gl.UNSIGNED_BYTE,
+        canvas,
+      );
+      errorCode = gl.getError();
+    });
+
+    if (errorCode !== 0) { console.log('renderingError, WebGL Error Code: ' + errorCode); }
+    gl.bindTexture(gl.TEXTURE_CUBE_MAP, null);
+  },
+
   tick: function () {
-    if (!this.texture || !this.el.sceneEl.xrSession) { return; }
-    if (!this.quadLayer) { this.initQuadLayer(); }
+    if (!this.el.sceneEl.xrSession) { return; }
+    if (!this.layer && this.el.sceneEl.is('vr-mode')) { this.initLayer(); }
     this.updateTransform();
-    if (!this.needsRedraw && !this.data.updatesEveryFrame) { return; }
+    if (!this.needsRedraw && !this.layer.needsRedraw && !this.textureIsVideo) { return; }
     this.draw();
     this.needsRedraw = false;
+  },
+
+  initLayer: function () {
+    var type = this.data.type;
+    if (type === 'quad') {
+      this.initQuadLayer();
+      return;
+    }
+
+    if (type === 'monocubemap' || type === 'stereocubemap') {
+      this.initCubeMapLayer();
+      return;
+    }
   },
 
   initQuadLayer: function () {
     var sceneEl = this.el.sceneEl;
     var gl = sceneEl.renderer.getContext();
-
-    this.xrGLFactory = new XRWebGLBinding(sceneEl.xrSession, gl);
-    this.quadLayer = this.xrGLFactory.createQuadLayer('texture', {
+    var xrGLFactory = this.xrGLFactory = new XRWebGLBinding(sceneEl.xrSession, gl);
+    if (!this.texture) { return; }
+    this.layer = xrGLFactory.createQuadLayer('texture', {
       space: this.referenceSpace,
-      viewPixelHeight: this.texture.image.height,
-      viewPixelWidth: this.texture.image.width,
+      viewPixelHeight: 2048,
+      viewPixelWidth: 2048,
       height: this.texture.image.height,
       width: this.texture.image.width
     });
-    sceneEl.renderer.xr.addLayer(this.quadLayer);
+    sceneEl.renderer.xr.addLayer(this.layer);
+  },
+
+  initCubeMapLayer: function () {
+    var src = this.data.src;
+    var sceneEl = this.el.sceneEl;
+    var gl = sceneEl.renderer.getContext();
+    var glSizeLimit = gl.getParameter(gl.MAX_CUBE_MAP_TEXTURE_SIZE);
+    var cubeFaceSize = this.cubeFaceSize = Math.min(glSizeLimit, Math.min(src.width, src.height));
+    var xrGLFactory = this.xrGLFactory = new XRWebGLBinding(sceneEl.xrSession, gl);
+    this.layer = xrGLFactory.createCubeLayer({
+      space: this.referenceSpace,
+      viewPixelWidth: cubeFaceSize,
+      viewPixelHeight: cubeFaceSize,
+      layout: this.data.type === 'monocubemap' ? 'mono' : 'stereo',
+      isStatic: true
+    });
+    this.loadCubeMapImages();
+  },
+
+  destroyLayer: function () {
+    if (!this.layer) { return; }
+    this.el.sceneEl.renderer.xr.removeLayer(this.layer);
+    this.layer.destroy();
+    this.layer = undefined;
+  },
+
+  updateQuadPanel: function () {
+    var quadPanelEl = this.quadPanelEl;
+    if (!this.quadPanelEl) {
+      quadPanelEl = this.quadPanelEl = document.createElement('a-entity');
+      this.el.appendChild(quadPanelEl);
+    }
+
+    quadPanelEl.setAttribute('material', {
+      shader: 'flat',
+      src: this.data.src,
+      transparent: true
+    });
+
+    quadPanelEl.setAttribute('geometry', {
+      primitive: 'plane',
+      height: this.texture.image.height / 1000,
+      width: this.texture.image.width / 1000
+    });
   },
 
   draw: function () {
     var sceneEl = this.el.sceneEl;
     var gl = this.el.sceneEl.renderer.getContext();
-    var glayer = this.xrGLFactory.getSubImage(this.quadLayer, sceneEl.frame);
+    var glayer = this.xrGLFactory.getSubImage(this.layer, sceneEl.frame);
     var texture = sceneEl.renderer.properties.get(this.texture).__webglTexture;
     var previousFrameBuffer = gl.getParameter(gl.FRAMEBUFFER_BINDING);
 
@@ -86,7 +229,8 @@ module.exports.Component = registerComponent('layer', {
     gl.bindFramebuffer(gl.FRAMEBUFFER, this.frameBuffer);
     gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, glayer.colorTexture, 0);
 
-    drawQuad(gl, texture);
+    // drawQuad(gl, texture, this.data.src);
+    blitTexture(gl, texture, glayer, this.data.src);
 
     gl.bindFramebuffer(gl.FRAMEBUFFER, previousFrameBuffer);
   },
@@ -98,7 +242,7 @@ module.exports.Component = registerComponent('layer', {
     el.object3D.updateMatrixWorld();
     position.setFromMatrixPosition(el.object3D.matrixWorld);
     quaternion.setFromRotationMatrix(el.object3D.matrixWorld);
-    this.quadLayer.transform = new XRRigidTransform(position, quaternion);
+    this.layer.transform = new XRRigidTransform(position, quaternion);
   },
 
   onEnterVR: function () {
@@ -113,6 +257,16 @@ module.exports.Component = registerComponent('layer', {
     if (texture) { initQuad(gl); }
     xrSession.requestReferenceSpace('local').then(this.onRequestedReferenceSpace);
     this.needsRedraw = true;
+    if (this.quadPanelEl) {
+      this.quadPanelEl.object3D.visible = false;
+    }
+    if (this.data.src.play) { this.data.src.play(); }
+  },
+
+  onExitVR: function () {
+    if (this.quadPanelEl) {
+      this.quadPanelEl.object3D.visible = true;
+    }
   },
 
   onRequestedReferenceSpace: function (referenceSpace) {
@@ -130,6 +284,9 @@ var indices = [
 ];
 
 function initQuad (gl) {
+  var previousArrayBuffer = gl.getParameter(gl.ARRAY_BUFFER_BINDING);
+  var previousElementArrayBuffer = gl.getParameter(gl.ELEMENT_ARRAY_BUFFER_BINDING);
+
   // x, y, z, u, v
   var vertices = [
     -1.0, 1.0, 0.0, 0, 0,  // bottom-left
@@ -161,9 +318,9 @@ function initQuad (gl) {
   gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
 
   // Unbind the buffer
-  gl.bindBuffer(gl.ARRAY_BUFFER, null);
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, previousElementArrayBuffer);
   // Unbind the buffer
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+  gl.bindBuffer(gl.ARRAY_BUFFER, previousArrayBuffer);
 
   // Vertex shader source code
   var vertCode =
@@ -219,69 +376,97 @@ function initQuad (gl) {
   gl.linkProgram(shaderProgram);
 }
 
-function drawQuad (gl, texture) {
-  var previousProgram = gl.getParameter(gl.CURRENT_PROGRAM);
-  var previousTexture;
-  var previousCullFace = gl.getParameter(gl.CULL_FACE_MODE);
-  var previousArrayBuffer = gl.getParameter(gl.ARRAY_BUFFER_BINDING);
-  var previousElementArrayBuffer = gl.getParameter(gl.ELEMENT_ARRAY_BUFFER_BINDING);
+function blitTexture (gl, texture, subImage, textureEl) {
+  var xrReadFramebuffer = gl.createFramebuffer();
+  let x1offset = subImage.viewport.x;
+  let y1offset = subImage.viewport.y;
+  let x2offset = subImage.viewport.x + subImage.viewport.width;
+  let y2offset = subImage.viewport.y + subImage.viewport.height;
 
-  // Use the combined shader program object
-  gl.useProgram(shaderProgram);
+  // Update video texture.
+  if (textureEl.tagName === 'VIDEO') {
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texSubImage2D(gl.TEXTURE_2D, 0, 0, 0, textureEl.width, textureEl.height, gl.RGB, gl.UNSIGNED_BYTE, textureEl);
+  }
 
-  // Get the attribute location
-  var coordinates = gl.getAttribLocation(shaderProgram, 'coordinates');
-  // Bind vertex buffer object
-  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-  // Point an attribute to the currently bound VBO
-  gl.vertexAttribPointer(coordinates, 3, gl.FLOAT, false, 20, 0);
-  // Enable the attribute
-  gl.enableVertexAttribArray(coordinates);
+  // Bind texture to read framebuffer.
+  gl.bindFramebuffer(gl.READ_FRAMEBUFFER, xrReadFramebuffer);
+  gl.framebufferTexture2D(gl.READ_FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture, 0);
 
-  // Get the attribute location
-  var texCoordinates = gl.getAttribLocation(shaderProgram, 'texCoordinates');
-  // Bind index buffer object
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-  // Point an attribute to the currently bound VBO
-  gl.vertexAttribPointer(texCoordinates, 2, gl.FLOAT, false, 20, 12);
-  // Enable the attribute
-  gl.enableVertexAttribArray(texCoordinates);
+  // Blit into layer buffer.
+  gl.readBuffer(gl.COLOR_ATTACHMENT0);
+  gl.blitFramebuffer(0, textureEl.height, textureEl.width, 0, x1offset, y1offset, x2offset, y2offset, gl.COLOR_BUFFER_BIT, gl.NEAREST);
 
-  var uSampler = gl.getUniformLocation(shaderProgram, 'uSampler');
-  // Tell WebGL we want to affect texture unit 0
-  gl.activeTexture(gl.TEXTURE0);
-  // Get previously bound texture.
-  previousTexture = gl.getParameter(gl.TEXTURE_BINDING_2D);
-  // Bind the texture to texture unit 0
-  gl.bindTexture(gl.TEXTURE_2D, texture);
-  // Tell the shader we bound the texture to texture unit 0
-  gl.uniform1i(uSampler, 0);
-
-  /* ============= Drawing the Quad ================ */
-
-  gl.enable(gl.CULL_FACE);
-  gl.cullFace(gl.BACK);
-
-  // Clear the canvas
-  gl.clearColor(1.0, 0, 0, 1.0);
-  // Enable the depth test
-  gl.enable(gl.DEPTH_TEST);
-  // Clear the color buffer bit
-  gl.clear(gl.COLOR_BUFFER_BIT);
-
-  // Draw the triangle
-  gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
-
-  // Unbind
-  gl.bindTexture(gl.TEXTURE_2D, previousTexture);
-
-  // Unbind
-  gl.bindBuffer(gl.ARRAY_BUFFER, previousArrayBuffer);
-  // Unbind index buffer object
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, previousElementArrayBuffer);
-
-  gl.cullFace(previousCullFace);
-
-  // Restore previous program
-  gl.useProgram(previousProgram);
+  gl.bindFramebuffer(gl.READ_FRAMEBUFFER, null);
+  gl.deleteFramebuffer(xrReadFramebuffer);
 }
+
+// function drawQuad (gl, texture, src) {
+//   var previousProgram = gl.getParameter(gl.CURRENT_PROGRAM);
+//   var previousTexture;
+//   var previousCullFace = gl.getParameter(gl.CULL_FACE_MODE);
+//   var previousArrayBuffer = gl.getParameter(gl.ARRAY_BUFFER_BINDING);
+//   var previousElementArrayBuffer = gl.getParameter(gl.ELEMENT_ARRAY_BUFFER_BINDING);
+
+//   // Use the combined shader program object
+//   gl.useProgram(shaderProgram);
+
+//   // Get the attribute location
+//   var coordinates = gl.getAttribLocation(shaderProgram, 'coordinates');
+//   // Bind vertex buffer object
+//   gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+//   // Point an attribute to the currently bound VBO
+//   gl.vertexAttribPointer(coordinates, 3, gl.FLOAT, false, 20, 0);
+//   // Enable the attribute
+//   gl.enableVertexAttribArray(coordinates);
+
+//   // Get the attribute location
+//   var texCoordinates = gl.getAttribLocation(shaderProgram, 'texCoordinates');
+//   // Bind index buffer object
+//   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+//   // Point an attribute to the currently bound VBO
+//   gl.vertexAttribPointer(texCoordinates, 2, gl.FLOAT, false, 20, 12);
+//   // Enable the attribute
+//   gl.enableVertexAttribArray(texCoordinates);
+
+//   var uSampler = gl.getUniformLocation(shaderProgram, 'uSampler');
+//   // Tell WebGL we want to affect texture unit 0
+//   gl.activeTexture(gl.TEXTURE0);
+//   // Get previously bound texture.
+//   previousTexture = gl.getParameter(gl.TEXTURE_BINDING_2D);
+//   // Bind the texture to texture unit 0
+//   gl.bindTexture(gl.TEXTURE_2D, texture);
+
+//   // gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, src);
+
+//   // Tell the shader we bound the texture to texture unit 0
+//   gl.uniform1i(uSampler, 0);
+
+//   /* ============= Drawing the Quad ================ */
+
+//   gl.enable(gl.CULL_FACE);
+//   gl.cullFace(gl.BACK);
+
+//   // Clear the canvas
+//   gl.clearColor(1.0, 0, 0, 1.0);
+//   // Enable the depth test
+//   gl.enable(gl.DEPTH_TEST);
+//   // Clear the color buffer bit
+//   gl.clear(gl.COLOR_BUFFER_BIT);
+
+//   // Draw the triangle
+//   gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
+
+//   // Unbind
+//   gl.bindTexture(gl.TEXTURE_2D, previousTexture);
+
+//   // Unbind
+//   gl.bindBuffer(gl.ARRAY_BUFFER, previousArrayBuffer);
+//   // Unbind index buffer object
+//   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, previousElementArrayBuffer);
+
+//   gl.cullFace(previousCullFace);
+
+//   // Restore previous program
+//   gl.useProgram(previousProgram);
+// }
