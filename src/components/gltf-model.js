4a4fad20401ebff36f188289ac3776e0fd44d268
Don McCurdy <don.r.mccurdy@gmail.com>
2021-08-23 23:21:59 -0700

    [gltf-model] Support EXT_meshopt_compression

--- src/components/gltf-model.js
+++ src/components/gltf-model.js
@@ -10,11 +10,20 @@ module.exports.Component = registerComponent('gltf-model', {
   schema: {type: 'model'},
 
   init: function () {
+    var self = this;
     var dracoLoader = this.system.getDRACOLoader();
+    var meshoptDecoder = this.system.getMeshoptDecoder();
     this.model = null;
     this.loader = new THREE.GLTFLoader();
     if (dracoLoader) {
       this.loader.setDRACOLoader(dracoLoader);
+    }
+    if (meshoptDecoder) {
+      this.ready = meshoptDecoder.then(function (meshoptDecoder) {
+        self.loader.setMeshoptDecoder(meshoptDecoder);
+      });
+    } else {
+      this.ready = Promise.resolve();
     }
   },
 
@@ -27,15 +36,17 @@ module.exports.Component = registerComponent('gltf-model', {
 
     this.remove();
 
-    this.loader.load(src, function gltfLoaded (gltfModel) {
-      self.model = gltfModel.scene || gltfModel.scenes[0];
-      self.model.animations = gltfModel.animations;
-      el.setObject3D('mesh', self.model);
-      el.emit('model-loaded', {format: 'gltf', model: self.model});
-    }, undefined /* onProgress */, function gltfFailed (error) {
-      var message = (error && error.message) ? error.message : 'Failed to load glTF model';
-      warn(message);
-      el.emit('model-error', {format: 'gltf', src: src});
+    this.ready.then(function () {
+      self.loader.load(src, function gltfLoaded (gltfModel) {
+        self.model = gltfModel.scene || gltfModel.scenes[0];
+        self.model.animations = gltfModel.animations;
+        el.setObject3D('mesh', self.model);
+        el.emit('model-loaded', {format: 'gltf', model: self.model});
+      }, undefined /* onProgress */, function gltfFailed (error) {
+        var message = (error && error.message) ? error.message : 'Failed to load glTF model';
+        warn(message);
+        el.emit('model-error', {format: 'gltf', src: src});
+      });
     });
   },
 
