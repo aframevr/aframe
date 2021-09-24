dde47021c5b9cd04b675eaa7be679dd58d6f9be2
Kieran Farr <kieran.farr@gmail.com>
2021-09-24 13:07:07 -0700

    update cursor.js to support ortho raycaster; fixes #4935

--- src/components/cursor.js
+++ src/components/cursor.js
@@ -221,8 +221,16 @@ module.exports.Component = registerComponent('cursor', {
       mouse.x = (left / bounds.width) * 2 - 1;
       mouse.y = -(top / bounds.height) * 2 + 1;
 
-      origin.setFromMatrixPosition(camera.matrixWorld);
-      direction.set(mouse.x, mouse.y, 0.5).unproject(camera).sub(origin).normalize();
+      if (camera && camera.isPerspectiveCamera) {
+        origin.setFromMatrixPosition(camera.matrixWorld);
+        direction.set(mouse.x, mouse.y, 0.5).unproject(camera).sub(origin).normalize();
+      } else if (camera && camera.isOrthographicCamera) {
+        origin.set(mouse.x, mouse.y, (camera.near + camera.far) / (camera.near - camera.far)).unproject(camera); // set origin in plane of camera
+        direction.set(0, 0, -1).transformDirection(camera.matrixWorld);
+      } else {
+        console.error('AFRAME.Raycaster: Unsupported camera type: ' + camera.type);
+      }
+
       this.el.setAttribute('raycaster', rayCasterConfig);
       if (evt.type === 'touchmove') { evt.preventDefault(); }
     };
