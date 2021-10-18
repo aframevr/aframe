a2aacc8b1458bdc02a2a67a5048c31a922cfedf1
a-frobot <aframebot@gmail.com>
2021-10-18 19:41:16 +0000

    Bump aframe-master dist/ builds. (https://github.com/aframevr/aframe/compare/94d5a97e708c...b1b133810686)

--- dist/aframe-master.js
+++ dist/aframe-master.js
@@ -61498,15 +61498,16 @@ module.exports.Component = register('background', {
     color: { type: 'color', default: 'black' },
     transparent: { default: false },
     generateEnvironment: { default: true },
-    environmentUpdateFrequency: { default: 0 },
     directionalLight: { type: 'selector' }
   },
   init: function () {
     var self = this;
 
-    this.cubeRenderTarget = new THREE.WebGLCubeRenderTarget(128, { format: THREE.RGBFormat, generateMipmaps: true, minFilter: THREE.LinearMipmapLinearFilter });
+    this.pmremGenerator = new THREE.PMREMGenerator(this.el.renderer);
+    this.pmremGenerator.compileCubemapShader();
+    this.cubeRenderTarget = new THREE.WebGLCubeRenderTarget(256, { format: THREE.RGBFormat, generateMipmaps: true, minFilter: THREE.LinearMipmapLinearFilter });
     this.lightProbeTarget = new THREE.WebGLCubeRenderTarget(16, { format: THREE.RGBFormat, generateMipmaps: false });
-    this.cubeCamera = new THREE.CubeCamera(1, 100000, this.cubeRenderTarget);
+    this.cubeCamera = new THREE.CubeCamera(0.1, 1000, this.cubeRenderTarget);
     this.needsEnvironmentUpdate = true;
     this.timeSinceUpdate = 0;
 
@@ -61543,7 +61544,6 @@ module.exports.Component = register('background', {
     }
 
     if (data.generateEnvironment) {
-      scene.environment = this.cubeRenderTarget.texture;
       this.needsEnvironmentUpdate = true;
     } else {
       scene.environment = null;
@@ -61619,15 +61619,7 @@ module.exports.Component = register('background', {
       object3D.background = new THREE.Color(data.color);
     }
 
-    if (scene.environment &&
-      (scene.environment !== this.cubeRenderTarget.texture || scene.environment !== this.lightProbeTarget.texture)
-    ) {
-      console.warn('Background will not override user defined environment maps');
-      return;
-    }
-
     if (data.generateEnvironment) {
-      scene.environment = this.cubeRenderTarget.texture;
       this.needsEnvironmentUpdate = true;
     } else {
       scene.environment = null;
@@ -61644,19 +61636,10 @@ module.exports.Component = register('background', {
     }
   },
 
-  tick: function (time, delta) {
+  tick: function () {
     var scene = this.el.object3D;
     var renderer = this.el.renderer;
     var frame = this.el.sceneEl.frame;
-
-    this.timeSinceUpdate += delta;
-    if (
-      this.data.environmentUpdateFrequency > 0 && // should update in general?
-      this.timeSinceUpdate > (this.data.environmentUpdateFrequency * 1000) // should update this tick?
-    ) {
-      this.timeSinceUpdate = 0;
-      this.needsEnvironmentUpdate = true;
-    }
 
     if (frame && this.xrLightProbe) {
       // light estimate may not yet be available, it takes a few frames to start working
@@ -61689,9 +61672,12 @@ module.exports.Component = register('background', {
     if (this.xrLightProbe) {
       this.updateXRCubeMap();
     } else {
-      this.el.object3D.add(this.cubeCamera);
       this.cubeCamera.position.set(0, 1.6, 0);
+      scene.environment = null;
       this.cubeCamera.update(renderer, scene);
+      scene.environment = this.pmremGenerator.fromCubemap(
+        this.cubeRenderTarget.texture
+      ).texture;
     }
 
     this.needsEnvironmentUpdate = false;
@@ -61704,9 +61690,7 @@ module.exports.Component = register('background', {
       object3D.background = null;
       return;
     }
-    if (object3D.environment === this.cubeRenderTarget.texture) {
-      object3D.environment = null;
-    }
+    this.pmremGenerator.dispose();
     object3D.background = COMPONENTS[this.name].schema.color.default;
   }
 });
@@ -71457,7 +71441,7 @@ require('./core/a-mixin');
 require('./extras/components/');
 require('./extras/primitives/');
 
-console.log('A-Frame Version: 1.2.0 (Date 2021-10-14, Commit #1d4854dd)');
+console.log('A-Frame Version: 1.2.0 (Date 2021-10-18, Commit #b1b13381)');
 console.log('THREE Version (https://github.com/supermedium/three.js):',
             pkg.dependencies['super-three']);
 console.log('WebVR Polyfill Version:', pkg.dependencies['webvr-polyfill']);
