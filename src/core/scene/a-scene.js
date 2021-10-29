93c51775e421e4200590eb464f0faf4b9deb882f
Christoph Bartschat <cmbartschat@gmail.com>
2021-10-29 09:16:50 -0700

    Rethrow XR request session error

--- src/core/scene/a-scene.js
+++ src/core/scene/a-scene.js
@@ -309,7 +309,9 @@ class AScene extends AEntity {
             function requestFail (error) {
               var useAR = xrMode === 'immersive-ar';
               var mode = useAR ? 'AR' : 'VR';
-              throw new Error('Failed to enter ' + mode + ' mode (`requestSession`) ' + error);
+              reject(
+                new Error('Failed to enter ' + mode + ' mode (`requestSession`) ' + error)
+              );
             }
           );
         });
