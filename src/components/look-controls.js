dfae67c2b465cabf283cf4256f199ed1e331be3e
Abdurrahman Rajab <rajab-2@windowslive.com>
2021-11-11 11:50:15 +0300

    fix(deltaX): adding deltaX to the calculations.

--- src/components/look-controls.js
+++ src/components/look-controls.js
@@ -359,11 +359,12 @@ module.exports.Component = registerComponent('look-controls', {
     if (!this.touchStarted || !this.data.touchEnabled) { return; }
 
     deltaY = 2 * Math.PI * (evt.touches[0].pageX - this.touchStart.x) / canvas.clientWidth;
+    deltaX = 2 * Math.PI * (evt.touches[0].pageY - this.touchStart.y) / canvas.clientHeight;
 
     direction = this.data.reverseTouchDrag ? 1 : -1;
     // Limit touch orientaion to to yaw (y axis).
     yawObject.rotation.y -= deltaY * 0.5 * direction;
-    pitchObject.rotation.x -= deltaY * 0.5 * direction;
+    pitchObject.rotation.x -= deltaX * 0.5 * direction;
     this.touchStart = {
       x: evt.touches[0].pageX,
       y: evt.touches[0].pageY
