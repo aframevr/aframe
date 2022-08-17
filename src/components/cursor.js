d5fd5ebe16513b931d4278275cf9f61a2c67e34b
diarmidmackenzie <diarmid.mackenzie@gmail.com>
2022-08-17 16:04:55 +0100

    Fix lint error arising from merge

--- src/components/cursor.js
+++ src/components/cursor.js
@@ -483,7 +483,6 @@ module.exports.Component = registerComponent('cursor', {
     function addOriginalEvent (detail, evt) {
       if (originalEvent instanceof MouseEvent) {
         detail.mouseEvent = originalEvent;
-
       } else if (typeof TouchEvent !== 'undefined' &&
                  originalEvent instanceof TouchEvent) {
         detail.touchEvent = originalEvent;
