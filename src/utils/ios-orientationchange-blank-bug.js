54022b80c60b12dc755f4f4a71a779b410dd23d0
Diego Marcos <diego.marcos@gmail.com>
2022-08-31 21:45:08 +0200

    Remove unnecessary template literal

--- src/utils/ios-orientationchange-blank-bug.js
+++ src/utils/ios-orientationchange-blank-bug.js
@@ -1,9 +1,9 @@
 // Safari regression introduced in iOS 12 and remains in iOS 13.
 // https://stackoverflow.com/questions/62717621/white-space-at-page-bottom-after-device-rotation-in-ios-safari
 window.addEventListener('orientationchange', function () {
-  document.documentElement.style.height = `initial`;
+  document.documentElement.style.height = 'initial';
   setTimeout(function () {
-    document.documentElement.style.height = `100%`;
+    document.documentElement.style.height = '100%';
     setTimeout(function () {
       // this line prevents the content
       // from hiding behind the address bar
