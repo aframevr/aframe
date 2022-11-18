84a0d636b1260a26f3288b1db8b41d1f2139409a
diarmidmackenzie <diarmid.mackenzie@gmail.com>
2022-11-18 19:13:13 +0000

    Fix test script that fails due to inadequate mocking of canvas.

--- tests/core/scene/a-scene.test.js
+++ tests/core/scene/a-scene.test.js
@@ -162,6 +162,10 @@ suite('a-scene (without renderer)', function () {
 
       // mock canvas
       sceneEl.canvas = {
+        classList: {
+          add: function () {},
+          remove: function () {}
+        },
         addEventListener: function () {},
         removeEventListener: function () {},
         requestFullscreen: function () {}
