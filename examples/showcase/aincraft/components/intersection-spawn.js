00042e0e8f70683118a74beab76d0fb2288b248c
diarmidmackenzie <diarmid.mackenzie@gmail.com>
2023-01-12 11:17:39 +0000

    Components from glitch

---
+++ examples/showcase/aincraft/components/intersection-spawn.js
@@ -0,0 +1,36 @@
+/* global AFRAME */
+
+/**
+ * Spawn entity at the intersection point on click, given the properties passed.
+ *
+ * `<a-entity intersection-spawn="mixin: box; material.color: red">` will spawn
+ * `<a-entity mixin="box" material="color: red">` at intersection point.
+ */
+AFRAME.registerComponent('intersection-spawn', {
+  schema: {
+    default: '',
+    parse: AFRAME.utils.styleParser.parse
+  },
+
+  init: function () {
+    const data = this.data;
+    const el = this.el;
+
+    el.addEventListener(data.event, evt => {
+      // Create element.
+      const spawnEl = document.createElement('a-entity');
+
+      // Snap intersection point to grid and offset from center.
+      spawnEl.setAttribute('position', evt.detail.intersection.point);
+
+      // Set components and properties.
+      Object.keys(data).forEach(name => {
+        if (name === 'event') { return; }
+        AFRAME.utils.entity.setComponentProperty(spawnEl, name, data[name]);
+      });
+
+      // Append to scene.
+      el.sceneEl.appendChild(spawnEl);
+    });
+  }
+});
