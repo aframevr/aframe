dc9ea65905a448fdc10fc029f4a27b5a8117752e
a-frobot <aframebot@gmail.com>
2023-02-26 12:34:06 +0000

    Bump aframe-master dist/ builds. (https://github.com/aframevr/aframe/compare/ddba34f4be78...3acb6cb03dd5)

--- src/index.js
+++ src/index.js
@@ -92,7 +92,7 @@ require('./core/a-mixin');
 require('./extras/components/');
 require('./extras/primitives/');
 
-console.log('A-Frame Version: 1.4.1 (Date 2023-02-13, Commit #7e693f72)');
+console.log('A-Frame Version: 1.4.1 (Date 2023-02-26, Commit #3acb6cb0)');
 console.log('THREE Version (https://github.com/supermedium/three.js):',
             pkg.dependencies['super-three']);
 console.log('WebVR Polyfill Version:', pkg.dependencies['webvr-polyfill']);
