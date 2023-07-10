c7ed63a80fb1f24ce1f4e47e447501e3fe06a44e
diarmidmackenzie <diarmid.mackenzie@gmail.com>
2023-07-10 20:45:26 +0100

    Constants in alphabetical order

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,7 @@
 module.exports = {
+  AFRAME_CDN_ROOT: window.AFRAME_CDN_ROOT || 'https://cdn.aframe.io/',
   AFRAME_INJECTED: 'aframe-injected',
   DEFAULT_CAMERA_HEIGHT: 1.6,
   DEFAULT_HANDEDNESS: 'right',
-  keyboardevent: require('./keyboardevent'),
-  AFRAME_CDN_ROOT: window.AFRAME_CDN_ROOT || 'https://cdn.aframe.io/'
+  keyboardevent: require('./keyboardevent')
 };
