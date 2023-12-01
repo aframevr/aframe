70f0899c06222d651efd0e61c2d1efabcbd4d586
Diego Marcos Segura <diego.marcos@gmail.com>
2023-12-01 11:13:11 -0800

    Try to fix tests on Github. Succeed on local

--- tests/components/hand-tracking-controls.test.js
+++ tests/components/hand-tracking-controls.test.js
@@ -68,8 +68,6 @@ suite('tracked-controls-webxr', function () {
   suite('children entities', function () {
     test('attached to the wrist joint', function (done) {
       var boxEl = document.createElement('a-box');
-      el.setAttribute('hand-tracking-controls', {hand: 'left'});
-      el.components['hand-tracking-controls'].checkIfControllerPresent();
       el.addEventListener('child-attached', function () {
         assert.equal(boxEl.object3D.parent, el.components['hand-tracking-controls'].wristObject3D);
         done();
@@ -78,6 +76,8 @@ suite('tracked-controls-webxr', function () {
         assert.ok(el.components['hand-tracking-controls'].wristObject3D);
         el.appendChild(boxEl);
       });
+      el.setAttribute('hand-tracking-controls', {hand: 'left'});
+      el.components['hand-tracking-controls'].checkIfControllerPresent();
     });
   });
 
