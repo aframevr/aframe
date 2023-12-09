ee7ec30b313d92181186368a98d139c090b5fd6f
Noeri Huisman <8823461+mrxz@users.noreply.github.com>
2023-12-09 19:16:20 +0100

    Remove epsilon for scale as Three.js handles zero scale (#5404)
    Co-authored-by: Noeri Huisman <mrxz@users.noreply.github.com>

--- src/components/scale.js
+++ src/components/scale.js
@@ -1,8 +1,5 @@
 var registerComponent = require('../core/component').registerComponent;
 
-// Avoids triggering a zero-determinant which makes object3D matrix non-invertible.
-var zeroScale = 0.00001;
-
 module.exports.Component = registerComponent('scale', {
   schema: {
     type: 'vec3',
@@ -12,10 +9,7 @@ module.exports.Component = registerComponent('scale', {
   update: function () {
     var data = this.data;
     var object3D = this.el.object3D;
-    var x = data.x === 0 ? zeroScale : data.x;
-    var y = data.y === 0 ? zeroScale : data.y;
-    var z = data.z === 0 ? zeroScale : data.z;
-    object3D.scale.set(x, y, z);
+    object3D.scale.set(data.x, data.y, data.z);
   },
 
   remove: function () {
