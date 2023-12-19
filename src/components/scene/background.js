754b8ada8e7c692da0fdd58f71f857a530f48991
Noeri Huisman <mrxz@users.noreply.github.com>
2023-12-19 14:21:16 +0100

    Set `.background` null when removing `background` component

--- src/components/scene/background.js
+++ src/components/scene/background.js
@@ -1,6 +1,5 @@
 /* global THREE */
 var register = require('../../core/component').registerComponent;
-var COMPONENTS = require('../../core/component').components;
 
 module.exports.Component = register('background', {
   schema: {
@@ -19,12 +18,7 @@ module.exports.Component = register('background', {
   },
 
   remove: function () {
-    var data = this.data;
     var object3D = this.el.object3D;
-    if (data.transparent) {
-      object3D.background = null;
-      return;
-    }
-    object3D.background = COMPONENTS[this.name].schema.color.default;
+    object3D.background = null;
   }
 });
