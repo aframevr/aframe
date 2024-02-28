3e5e35c29c66ac07144ed046a928469ff995cc31
Vincent Fretin <vincent.fretin@gmail.com>
2024-02-28 18:39:13 +0100

    Update webpack and associated loaders, node 18.12.0 is now the minimum version required for webpack-dev-server, remove webpack-merge, webpack now supports extending another configuration

--- webpack.config.js
+++ webpack.config.js
@@ -42,9 +42,6 @@ module.exports = {
         test: /\.js$/,
         use: {
           loader: 'babel-loader'
-          // options: {
-          //   presets: ['@babel/preset-env'],
-          // },
         }
       },
       {
