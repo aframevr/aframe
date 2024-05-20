c683860710088c1e4a4b97839773aebeae616a2e
Vincent Fretin <vincent.fretin@gmail.com>
2024-05-20 15:29:42 +0200

    make the imports works in node 22 with --experimental-require-module flag

--- src/lib/three.mjs
+++ src/lib/three.mjs
@@ -1,17 +1,14 @@
 import * as SUPER_THREE from 'super-three';
-import { DRACOLoader } from 'super-three/examples/jsm/loaders/DRACOLoader';
-import { GLTFLoader } from 'super-three/examples/jsm/loaders/GLTFLoader';
-import { KTX2Loader } from 'super-three/examples/jsm/loaders/KTX2Loader';
+import { DRACOLoader } from 'super-three/examples/jsm/loaders/DRACOLoader.js';
+import { GLTFLoader } from 'super-three/examples/jsm/loaders/GLTFLoader.js';
+import { KTX2Loader } from 'super-three/examples/jsm/loaders/KTX2Loader.js';
 import { OBB } from 'super-three/addons/math/OBB.js';
-import { OBJLoader } from 'super-three/examples/jsm/loaders/OBJLoader';
-import { MTLLoader } from 'super-three/examples/jsm/loaders/MTLLoader';
-import * as BufferGeometryUtils from 'super-three/examples/jsm/utils/BufferGeometryUtils';
-import { LightProbeGenerator } from 'super-three/examples/jsm/lights/LightProbeGenerator';
+import { OBJLoader } from 'super-three/examples/jsm/loaders/OBJLoader.js';
+import { MTLLoader } from 'super-three/examples/jsm/loaders/MTLLoader.js';
+import * as BufferGeometryUtils from 'super-three/examples/jsm/utils/BufferGeometryUtils.js';
+import { LightProbeGenerator } from 'super-three/examples/jsm/lights/LightProbeGenerator.js';
 
-var THREE = window.THREE = SUPER_THREE;
-
-// TODO: Eventually include these only if they are needed by a component.
-require('../../vendor/DeviceOrientationControls'); // THREE.DeviceOrientationControls
+var THREE = window.THREE = { ...SUPER_THREE };
 THREE.DRACOLoader = DRACOLoader;
 THREE.GLTFLoader = GLTFLoader;
 THREE.KTX2Loader = KTX2Loader;
