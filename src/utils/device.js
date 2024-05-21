b9392ecfcbceff2ba6b714622b66adf9def220f2
Vincent Fretin <vincent.fretin@gmail.com>
2024-05-21 13:59:21 +0200

    put back a comment about mocked process in isBrowserEnvironment

--- src/utils/device.js
+++ src/utils/device.js
@@ -200,6 +200,7 @@ module.exports.isLandscape = function () {
  * We need to check a node api that isn't mocked on either side.
  * `require` and `module.exports` are mocked in browser by bundlers.
  * `window` is mocked in node.
+ * `process` is also mocked by webpack running with karma, but has custom properties like process.browser.
  */
 module.exports.isBrowserEnvironment = typeof process === 'undefined' || process.browser === true;
 
