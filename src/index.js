31b8384128155301539f2c4e7e2507d2aca17e88
a-frobot <aframebot@gmail.com>
2024-11-08 01:55:07 +0000

    Bump aframe-master dist/ builds. (https://github.com/aframevr/aframe/compare/e3fa46d8fc58...0344339a8a63)

--- src/index.js
+++ src/index.js
@@ -56,7 +56,7 @@ require('./core/a-mixin');
 require('./extras/components/');
 require('./extras/primitives/');
 
-console.log('A-Frame Version: 1.6.0 (Date 2024-11-02, Commit #eae4b7f4)');
+console.log('A-Frame Version: 1.6.0 (Date 2024-11-08, Commit #0344339a)');
 console.log('THREE Version (https://github.com/supermedium/three.js):',
             THREE.REVISION);
 
