b08a29d3d3a91e67644c2d3cabb090dd72ff2d2f
Enzo Francesca <99999751+enzofrancescaHM@users.noreply.github.com>
2025-02-01 14:30:53 +0100

    Update bloom.js to fix distorsions in VR Mode

--- examples/showcase/post-processing/bloom.js
+++ examples/showcase/post-processing/bloom.js
@@ -1,7 +1,8 @@
+/* global THREE */
 /**
  * Unreal Bloom Effect
  * Implementation for A-Frame
- * Code modified from Akbartus's UnrealBloomPass.js
+ * Code modified from Akbartus's post-processing A-Frame integration
  * https://github.com/akbartus/A-Frame-Component-Postprocessing
  */
 
@@ -10,77 +11,92 @@ import { EffectComposer } from "three/addons/postprocessing/EffectComposer.js";
 import { RenderPass } from "three/addons/postprocessing/RenderPass.js";
 import { UnrealBloomPass } from "three/addons/postprocessing/UnrealBloomPass.js";
 
-/////////////////////////////////////
-// A-FRAME COMPONENT: BLOOM        //
-/////////////////////////////////////
 AFRAME.registerComponent("bloom", {
   schema: {
     threshold: { type: "number", default: 0 },
     strength: { type: "number", default: 0.4 },
     radius: { type: "number", default: 0 },
   },
-  init: function () {
-    this.bind();
+  events: {
+    rendererresize: function () {
+      this.renderer.getSize(this.size);
+      this.composer.setSize(this.size.width, this.size.height);
+    },
+    "enter-vr": function () {
+      this.renderer.getSize(this.size);
+      this.composer.setSize(this.size.width, this.size.height);
+    },
   },
-  update: function (oldData) {
-    this.evaluateEffect();
-  },
-  evaluateEffect: function () {
+  init() {
+    this.size = new THREE.Vector2();
     this.scene = this.el.object3D;
     this.renderer = this.el.renderer;
     this.camera = this.el.camera;
-
-    // create composer with multisampling to avoid alias
+    this.bind();
+  },
+  update: function () {
+    if (this.composer) {
+      this.composer.dispose();
+    }
+    // create composer with multisampling to avoid aliasing
     const resolution = this.renderer.getDrawingBufferSize(new THREE.Vector2());
-    const renderTarget = new THREE.WebGLRenderTarget(resolution.width, resolution.height, { type: THREE.HalfFloatType, samples: 8 });
+    const renderTarget = new THREE.WebGLRenderTarget(
+      resolution.width,
+      resolution.height,
+      { type: THREE.HalfFloatType, samples: 8 }
+    );
+
     this.composer = new EffectComposer(this.renderer, renderTarget);
-    
+
     const renderScene = new RenderPass(this.scene, this.camera);
     this.composer.addPass(renderScene);
 
-    // creating bloom pass    
-    const bloomPass = new UnrealBloomPass(
-      new THREE.Vector2(
-        this.renderer.domElement.clientWidth,
-        this.renderer.domElement.clientHeight
-      ),
-      1.5,
-      0.4,
-      0.85
+    // creating bloom pass
+    const strength = this.data.strength;
+    const radius = this.data.radius;
+    const threshold = this.data.threshold;
+    if (this.bloomPass) {
+      this.bloomPass.dispose();
+    }
+    this.bloomPass = new UnrealBloomPass(
+      resolution,
+      strength,
+      radius,
+      threshold
     );
 
-    bloomPass.fsQuad.render = function (renderer) {
+    this.bloomPass.fsQuad.render = function (renderer) {
       // Disable XR projection for fullscreen effects
       // https://github.com/mrdoob/three.js/pull/18846
       const xrEnabled = renderer.xr.enabled;
-      const _camera = new THREE.OrthographicCamera(- 1, 1, 1, - 1, 0, 1);
+      const _camera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0, 1);
       renderer.xr.enabled = false;
       renderer.render(this._mesh, _camera);
       renderer.xr.enabled = xrEnabled;
-    }
+    };
 
-    bloomPass.threshold = this.data.threshold;
-    bloomPass.strength = this.data.strength;
-    bloomPass.radius = this.data.radius;
-    this.composer.addPass(bloomPass);
+    this.composer.addPass(this.bloomPass);
   },
 
   bind: function () {
-    const render = this.el.renderer.render;
+    this.originalRender = this.el.renderer.render;
     const self = this;
     let isInsideComposerRender = false;
 
     this.el.renderer.render = function () {
-
       if (isInsideComposerRender) {
-        render.apply(this, arguments);
+        self.originalRender.apply(this, arguments);
       } else {
         isInsideComposerRender = true;
-        self.composer.setSize( self.renderer.domElement.clientWidth / window.devicePixelRatio, self.renderer.domElement.clientHeight / window.devicePixelRatio );
         self.composer.render(self.el.sceneEl.delta / 1000);
         isInsideComposerRender = false;
       }
-
     };
+  },
+
+  remove() {
+    this.el.renderer.render = this.originalRender;
+    this.bloomPass.dispose();
+    this.composer.dispose();
   },
 });
