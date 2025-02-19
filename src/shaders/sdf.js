a66e6d2cbb18f31faaa589afc5048dea32e0988e
Noeri Huisman <8823461+mrxz@users.noreply.github.com>
2025-02-19 23:46:06 +0100

    Check for presence of UniformsLib/Utils instead of importing them from three/src (#5668)
    Co-authored-by: Diego Marcos <diego.marcos@gmail.com>

--- src/shaders/sdf.js
+++ src/shaders/sdf.js
@@ -1,7 +1,5 @@
 import { registerShader } from '../core/shader.js';
 import * as THREE from 'three';
-import { UniformsUtils } from 'three/src/renderers/shaders/UniformsUtils.js';
-import { UniformsLib } from 'three/src/renderers/shaders/UniformsLib.js';
 
 var VERTEX_SHADER = [
   '#include <common>',
@@ -96,10 +94,14 @@ export var Shader = registerShader('sdf', {
   fragmentShader: FRAGMENT_SHADER,
 
   init: function () {
-    this.uniforms = UniformsUtils.merge([
-      UniformsLib.fog,
-      this.initUniforms()
-    ]);
+    this.uniforms = this.initUniforms();
+    // When using the WebGPURenderer there is no UniformsLib or UniformsUtils.
+    if (THREE.UniformsUtils) {
+      this.uniforms = THREE.UniformsUtils.merge([
+        THREE.UniformsLib.fog,
+        this.uniforms
+      ]);
+    }
     this.material = new THREE.ShaderMaterial({
       uniforms: this.uniforms,
       vertexShader: this.vertexShader,
