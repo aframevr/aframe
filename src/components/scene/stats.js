77481e2acf1bcbab5506c24e909c5199b334ed05
arvind-iyer <arvindi.me@gmail.com>
2025-04-06 17:00:05 +0530

    allow stats ui to be toggled

--- src/components/scene/stats.js
+++ src/components/scene/stats.js
@@ -1,15 +1,35 @@
 import Stats from 'stats-gl';
 import { registerComponent } from '../../core/component.js';
 
+var HIDDEN_CLASS = 'a-hidden';
+
 registerComponent('stats', {
   schema: {default: true},
+
   sceneOnly: true,
+
   init: function () {
     this.stats = new Stats();
     this.stats.init(this.el.renderer);
     document.body.append(this.stats.dom);
   },
+
   tick: function () {
-    this.stats.update();
+    if (this.data) {
+      this.stats.update();
+    }
+  },
+
+  update: function () {
+    if (!this.stats) { return; }
+    return (!this.data) ? this.hide() : this.show();
+  },
+
+  hide: function () {
+    this.stats.dom.classList.add(HIDDEN_CLASS);
+  },
+
+  show: function () {
+    this.stats.dom.classList.remove(HIDDEN_CLASS);
   }
 });
